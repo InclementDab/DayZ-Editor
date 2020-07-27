9a3c8e9f0c1877e2f686fee23be3654fce17e875
InclementDeath <tylerpaul2329@gmail.com>
2020-07-27 05:11:23 -0400

    Updated compile

--- Compile.bat
+++ Compile.bat
@@ -11,7 +11,17 @@ for /f "tokens=2* skip=2" %%a in ('reg query %QUERY%  /V "exe"') do (
 	set BUILDEREXE="%BUILDEREXE%\%%b"
 )
 
+mkdir "%CD%\Addons\scripts\1_core"
+mkdir "%CD%\Addons\scripts\3_game"
+mkdir "%CD%\Addons\scripts\4_world"
+mkdir "%CD%\Addons\scripts\5_mission"
+
+robocopy /E "%CD%\1_core\dayzeditor" "%CD%\Addons\scripts\1_core"
+robocopy /E "%CD%\3_game\dayzeditor" "%CD%\Addons\scripts\3_game"
+robocopy /E "%CD%\4_world\dayzeditor" "%CD%\Addons\scripts\4_world"
+robocopy /E "%CD%\5_mission\dayzeditor" "%CD%\Addons\scripts\5_mission"
+
 %BUILDEREXE% "%CD%\Addons\editor" "%CD%\Addons" -prefix=DayZEditor\editor
 %BUILDEREXE% "%CD%\Addons\gui" "%CD%\Addons" -prefix=DayZEditor\gui
-
 %BUILDEREXE% "%CD%\Addons\scripts" "%CD%\Addons" -prefix=DayZEditor\scripts
+pause
