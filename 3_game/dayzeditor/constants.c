dd2db13dcd7a34fbd11a4432e1cae40fe339cf03
InclementDeath <tylerpaul2329@gmail.com>
2020-07-29 15:53:38 -0400

    update constants cause im not very smart

--- 3_game/dayzeditor/constants.c
+++ 3_game/dayzeditor/constants.c
@@ -1,9 +1,9 @@
 
-#ifdef EDITOR_RELEASE
+
 static string layout_dir = "DayZEditor/gui/Layouts/";
-#else
-static string layout_dir = "P:/DayZ_Server/dev/DayZEditor/Addons/gui/Layouts/";
-#endif
+
+//static string layout_dir = "P:/DayZ_Server/dev/DayZEditor/Addons/gui/Layouts/";
+
 
 
 
