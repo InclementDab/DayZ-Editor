d131228b8ba499d5b6ca242dcf9b4242a9666345
Salutesh <stephan_moser@gmx.de>
2020-08-11 21:37:20 +0200

    More prep and icons

--- DayZEditor/scripts/3_game/dayzeditor/constants.c
+++ DayZEditor/scripts/3_game/dayzeditor/constants.c
@@ -1,6 +1,12 @@
 
+//! Folders
+static const string EDITOR_GUI_FOLDER = "DayZEditor\\gui\\";
+static const string EDITOR_GUI_ICONS_FOLDER = EDITOR_GUI_FOLDER + "icons\\";
 
-
+//! Icons
+static const string EDITOR_ICON_FOLDER = EDITOR_GUI_ICONS_FOLDER + "icon_folder_128x128.edds";
+static const string EDITOR_ICON_SAVE = EDITOR_GUI_ICONS_FOLDER + "icon_folder_save_128x128.edds";
+static const string EDITOR_ICON_OPEN = EDITOR_GUI_ICONS_FOLDER + "icon_folder_open_128x128.edds";
 
 
 
