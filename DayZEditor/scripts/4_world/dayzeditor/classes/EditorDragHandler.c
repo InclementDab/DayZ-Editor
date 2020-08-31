2388cf84d0c45602bccecbca4bbdf3c1c4989eb2
InclementDeath <tylerpaul2329@gmail.com>
2020-08-30 22:58:12 -0400

    good day smile :)

--- DayZEditor/scripts/4_world/dayzeditor/classes/EditorDragHandler.c
+++ DayZEditor/scripts/4_world/dayzeditor/classes/EditorDragHandler.c
@@ -22,7 +22,7 @@ class DragHandler
 		if (GetMouseState(MouseState.LEFT) & MB_PRESSED_MASK) {
 			vector transform[4];
 			m_EditorObject.GetTransform(transform);
-			OnDragging(transform);
+			OnDragging(transform, m_EditorObject);
 			m_EditorObject.SetTransform(transform);
 			m_EditorObject.Update();
 		} else {
@@ -30,7 +30,7 @@ class DragHandler
 		}
 	}
 	
-	void OnDragging(out vector transform[4]);
+	void OnDragging(out vector transform[4], notnull EditorObject target);
 	
 	
 	vector GetGroundPosition(vector transform[4])
@@ -46,7 +46,7 @@ class DragHandler
 
 class ObjectDragHandler: DragHandler
 {
-	override void OnDragging(out vector transform[4])
+	override void OnDragging(out vector transform[4], notnull EditorObject target)
 	{
 		vector begin_pos = GetGame().GetCurrentCameraPosition();
 		vector end_pos = begin_pos + GetGame().GetPointerDirection() * 3000;
@@ -92,68 +92,11 @@ class ObjectDragHandler: DragHandler
 				transform[3] = cursor_pos;
 				transform[3][1] = transform[3][1] + size[1];		
 			} 
-			
-			//transform[3] = cursor_pos;
-			//transform[3][1] = cursor_pos[1] + size[1]/2;
 		}
 		
-		
-		
-	}
-	
-	
-	/*
-	// Handles Marker Drag from Object Base Marker
-	void ObjectDragUpdate(notnull EditorObject target)
-	{
-
-		Input input = GetGame().GetInput();
-		if (input.LocalRelease("UAFire")) {
-			EditorEvents.DropInvoke(this, target);
-			return;
-		}
-		
-		// Get all object data 
-		vector object_size = target.GetSize();
-		vector object_transform[4], start_transform[4];
-		
-		// non-updated version of object_transform
-		target.GetTransform(start_transform); 
-		target.GetTransform(object_transform);
-		
-		
-		// Raycast ground below object
-		set<Object> o;
-		vector ground, ground_dir; int component;
-		DayZPhysics.RaycastRV(object_transform[3], object_transform[3] + object_transform[1] * -1000, ground, ground_dir, component, o, NULL, target.GetWorldObject(), false, true); // set to ground only
-
-		vector cursor_position = MousePosToRay(o, target.GetWorldObject(), EditorSettings.OBJECT_VIEW_DISTANCE, 0, !EditorSettings.COLLIDE_ON_DRAG);
-		vector surface_normal = GetGame().SurfaceGetNormal(ground[0], ground[2]);
-		float surface_level = GetGame().SurfaceY(ground[0], ground[2]);
-	
-		// debug
-		Editor.DebugObject0.SetPosition(cursor_position);
-
-		// Handle Z only motion
-		
-		if (input.LocalValue("UALookAround")) {	
-			
-		
-		// Handle XY Rotation
-		} else if (input.LocalValue("UATurbo")) {
-
-		
-		} else {			
-			
-		
-		}
-		
-	
-		target.SetTransform(object_transform);
-		target.Update();
 		
 		// This handles all other selected objects
-		EditorObjectSet selected_objects = GetObjectManager().GetSelectedObjects();
+		EditorObjectSet selected_objects = GetEditor().GetSelectedObjects();
 		foreach (EditorObject selected_object: selected_objects) {
 			
 			if (selected_object == target) continue;
@@ -161,23 +104,24 @@ class ObjectDragHandler: DragHandler
 			vector selected_transform[4];	
 			selected_object.GetTransform(selected_transform);
 			
-			vector pos_delta = selected_transform[3] - start_transform[3];
-			vector cursor_position_delta = object_transform[3] + selected_transform[3] - start_transform[3];
-			vector delta2 = start_transform[3] - selected_transform[3];
+			vector object_transform[4];
+			target.GetTransform(object_transform);
+			vector pos_delta = selected_transform[3] - transform[3];
+			vector cursor_position_delta = object_transform[3] + selected_transform[3] - transform[3];
+			vector delta2 = transform[3] - selected_transform[3];
 		
 			float angle_delta = Math.Atan2(pos_delta[0], pos_delta[2]) * Math.RAD2DEG;
 			surface_normal = GetGame().SurfaceGetNormal(selected_transform[3][0], selected_transform[3][2]);
-			DayZPhysics.RaycastRV(selected_transform[3], selected_transform[3] + selected_transform[1] * -1000, ground, ground_dir, component, o, NULL, selected_object.GetWorldObject(), false, true); // set to ground only
-
+			ground_position = GetGroundPosition(selected_transform);
 			
-			// Handle Z only motion for all children				
-			if (input.LocalValue("UALookAround")) {
+			// Handle Z-Only motion
+			if (KeyState(KeyCode.KC_LMENU)) {
 				// Source object position + delta
 				//selected_transform[3][1] = object_transform[3][1] + pos_delta[1];
-				selected_transform[3] = ground + selected_transform[1] * vector.Distance(cursor_position_delta, ground);
+				selected_transform[3] = ground_position + selected_transform[1] * vector.Distance(cursor_position_delta, ground_position);
 				
-			// Handle XY Rotation for all children
-			} else if (input.LocalValue("UATurbo")) {
+			// Handle XY Rotation
+			} else if (KeyState(KeyCode.KC_LSHIFT)) {
 				vector rot_pos;
 				angle -= angle_delta;				
 				vector new_postion = vector.RotateAroundZero(pos_delta, vector.Up, Math.Cos(angle), Math.Sin(angle));
@@ -188,10 +132,10 @@ class ObjectDragHandler: DragHandler
 				//cursor_position_delta[1] = ground[1];
 				if (EditorSettings.MAINTAIN_HEIGHT) {
 					if (EditorSettings.MAGNET_PLACEMENT) {
-						selected_transform[3] = cursor_position_delta + surface_normal * vector.Distance(ground, selected_transform[3]);
+						selected_transform[3] = cursor_position_delta + surface_normal * vector.Distance(ground_position, selected_transform[3]);
 					} else {
 					
-						selected_transform[3] = cursor_position_delta + selected_transform[1] * vector.Distance(ground, selected_transform[3]);
+						selected_transform[3] = cursor_position_delta + selected_transform[1] * vector.Distance(ground_position, selected_transform[3]);
 					}
 				} else {
 					selected_transform[3] = cursor_position_delta;				
@@ -208,19 +152,14 @@ class ObjectDragHandler: DragHandler
 			selected_object.Update();
 			
 		}
-		
-		
-		//GlobalTranslationWidget.UpdatePosition();
-		//Print(TickCount(starttime) / 1000);
 	}
-*/
 }
 
 
 class MapDragHandler: DragHandler
 {
 	
-	override void OnDragging(out vector transform[4])
+	override void OnDragging(out vector transform[4], notnull EditorObject target)
 	{
 		
 		
--- DayZEditor/scripts/4_world/dayzeditor/gui/Controllers/EditorObjectPropertiesController.c
+++ DayZEditor/scripts/4_world/dayzeditor/gui/Controllers/EditorObjectPropertiesController.c
@@ -12,8 +12,8 @@ class EditorPropertiesController: Controller
 		m_EditorObject = editor_object;
 		vector position = m_EditorObject.GetPosition();
 		pos_x = position[0].ToString();
-		pos_y = position[1].ToString();
-		pos_z = position[2].ToString();
+		pos_z = position[1].ToString();
+		pos_y = position[2].ToString();
 		
 		NotifyPropertyChanged("pos_x");
 		NotifyPropertyChanged("pos_y");
@@ -21,9 +21,9 @@ class EditorPropertiesController: Controller
 		
 		
 		vector orientation = m_EditorObject.GetOrientation();
+		rot_z = orientation[0].ToString();
 		rot_x = orientation[1].ToString();
-		rot_y = orientation[0].ToString();
-		rot_z = orientation[2].ToString();
+		rot_y = orientation[2].ToString();
 		
 		NotifyPropertyChanged("rot_x");
 		NotifyPropertyChanged("rot_y");
@@ -43,14 +43,14 @@ class EditorPropertiesController: Controller
 			case "pos_x":
 			case "pos_y":
 			case "pos_z": {
-				m_EditorObject.SetPosition(Vector(pos_x.Evaluate(), pos_y.Evaluate(), pos_z.Evaluate()));
+				m_EditorObject.SetPosition(Vector(pos_x.Evaluate(), pos_z.Evaluate(), pos_y.Evaluate()));
 				break;
 			}
 			
 			case "rot_x":
 			case "rot_y":
 			case "rot_z": {
-				m_EditorObject.SetOrientation(Vector(rot_y.Evaluate(), rot_x.Evaluate(), rot_z.Evaluate()));
+				m_EditorObject.SetOrientation(Vector(rot_z.Evaluate(), rot_x.Evaluate(), rot_y.Evaluate()));
 				break;
 			}
 			
@@ -73,7 +73,6 @@ class EditorPropertiesController: Controller
 		if (KeyState(KeyCode.KC_LCONTROL)) {
 			direction *= 10;
 		}
-		
 		
 		EnScript.SetClassVar(this, w_name, 0, (w_eval.Evaluate() + direction).ToString());
 		NotifyPropertyChanged(w_name);
