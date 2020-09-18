826d978c070695660064418d9da7e16f4225de09
InclementDeath <tylerpaul2329@gmail.com>
2020-09-18 05:41:24 -0400

    Lerping pog

--- DayZEditor/scripts/4_world/DayZEditor/classes/Environment/Environment.c
+++ DayZEditor/scripts/4_world/DayZEditor/classes/Environment/Environment.c
@@ -32,8 +32,14 @@ modded class Environment
 	private void StartBlowout(vector position)
 	{
 		PlayEnvironmentSound(BlowoutSound.Blowout_Begin, position);
-		thread LerpBlur(0.8, 0.1, 0.75);
+		SetCameraPostProcessEffect(0, 1, PostProcessEffectType.ChromAber, "");
+		
+	
+		thread LerpMaterialParam("graphics/materials/postprocess/gauss", "Intensity", 0.8, 0.1, 0.75);
+		thread LerpMaterialParam("graphics/materials/postprocess/glow", "Vignette", 0.9, 0.25, 0.75);
 		Sleep(500);
+		
+		
 	}
 	
 	private void StartAPSI()
@@ -63,17 +69,19 @@ modded class Environment
 		effect.SoundPlay();
 	}
 	
-	private void LerpBlur(float start, float finish, float duration)
+	// Call asynchronously
+	private void LerpMaterialParam(string material_name, string param, float start, float finish, float duration)
 	{
-		PPEffects.SetBlur(start);
-		
+		LerpMaterialParam(GetGame().GetWorld().GetMaterial(material_name), param, start, finish, duration);
+	}
+	
+	private void LerpMaterialParam(Material material, string param, float start, float finish, float duration)
+	{
 		int i = 0;
 		while (i < duration * 1000) {
-			PPEffects.SetBlur(Math.Lerp(start, finish, (1 / duration) * i / 1000));
+			material.SetParam(param, Math.Lerp(start, finish, (1 / duration) * i / 1000));
 			Sleep(1);
 			i += 10;
 		}
-		
-		PPEffects.SetBlur(finish);
 	}
 }
