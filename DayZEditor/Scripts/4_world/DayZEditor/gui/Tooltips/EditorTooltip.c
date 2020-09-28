3a81634fc425fbe0236f652bb571f9bf28ad81bf
InclementDeath <tylerpaul2329@gmail.com>
2020-09-27 21:59:17 -0400

    Moved EditorTooltip

--- DayZEditor/Scripts/4_world/DayZEditor/gui/Tooltips/EditorTooltip.c
+++ DayZEditor/Scripts/4_world/DayZEditor/gui/Tooltips/EditorTooltip.c
@@ -1,37 +1,4 @@
 
-class EditorTooltipController: Controller
-{
-	string ContentTitle;
-	string ContentText;
-	
-	Object ContentItemData;
-	
-	void ~EditorTooltipController()
-	{
-		EditorLog.Trace("~EditorTooltipController");
-#ifndef COMPONENT_SYSTEM
-		if (ContentItemData) 
-			GetWorkbenchGame().ObjectDelete(ContentItemData);
-#endif
-	}
-	
-	override void PropertyChanged(string property_name)
-	{
-		switch (property_name) {
-			
-			case "ContentItemData": {
-				if (ContentItemData && ContentItemData.GetDisplayName() != string.Empty) {
-					ContentTitle = ContentItemData.GetDisplayName();
-					NotifyPropertyChanged("ContentTitle");
-				}
-				
-				break;
-			}
-			
-		}
-	}
-}
-
 class EditorTooltip: ScriptView
 {
 	protected TextWidget HeaderLabel;
