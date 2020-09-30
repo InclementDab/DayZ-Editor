7b6de055d256254d2ac8c7ce07a557ab8a34b851
InclementDeath <tylerpaul2329@gmail.com>
2020-09-30 09:23:33 -0400

    Update EditorObjectPropertiesDialog.c

--- DayZEditor/Scripts/4_world/DayZEditor/gui/Dialogs/EditorObjectPropertiesDialog.c
+++ DayZEditor/Scripts/4_world/DayZEditor/gui/Dialogs/EditorObjectPropertiesDialog.c
@@ -3,9 +3,22 @@ class EditorObjectPropertiesDialogController: DialogBaseController
 {
 	vector position;
 	
+	protected EditorObject m_EditorObject;
+	
+	void SetEditorObject(EditorObject editor_object)
+	{
+		m_EditorObject = editor_object;
+	}
+	
 	override void PropertyChanged(string property_name)
 	{
-		Print(position);
+		switch (property_name) {
+			
+			case "position": {				
+				m_EditorObject.SetPosition(position);
+				break;
+			}
+		}
 	}
 }
 
@@ -13,8 +26,13 @@ class EditorObjectPropertiesDialogController: DialogBaseController
 
 class EditorObjectPropertiesDialog: EditorDialogBase
 {
+	protected EditorObjectPropertiesDialogController m_EditorObjectPropertiesDialogController;
+	
 	void EditorObjectPropertiesDialog(Widget parent = null, string title = "", EditorObject editor_object = null)
 	{		
+		m_EditorObjectPropertiesDialogController = EditorObjectPropertiesDialogController.Cast(GetController());
+		m_EditorObjectPropertiesDialogController.SetEditorObject(editor_object);
+		
 		GroupPrefab group_prefab = new GroupPrefab(null, "Position", m_Controller);
 		group_prefab.Insert(new VectorPrefab(null, "Position", m_Controller, "position", editor_object.GetPosition()));
 		
