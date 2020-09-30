2eda1690fb4cd499c306c7e2fd5f500355b9df62
InclementDeath <tylerpaul2329@gmail.com>
2020-09-30 07:38:11 -0400

    Split Exports into files

---
+++ DayZEditor/Scripts/4_world/DayZEditor/classes/FileManagers/EditorTerrainBuilderExport.c
@@ -0,0 +1,23 @@
+static FileDialogResult ExportTBData(ref EditorObjectDataSet data, string filename, ExportSettings export_settings)
+{
+	FileHandle handle = OpenFile(filename, FileMode.WRITE);
+	
+	if (!handle) {
+		return FileDialogResult.IN_USE;
+	}
+	
+	vector terrainbuilder_offset = Vector(200000, 0, 0);
+	foreach (EditorObjectData editor_object: data) {
+		// "construction_house2";206638.935547;6076.024414;146.000015;0.000000;0.000000;1.000000;
+		// Name, X, Y, Yaw, Pitch, Roll, Scale, Relative Height										
+		//if (height_type == HeightType.RELATIVE)
+		//position[1] = position[1] - GetGame().SurfaceY(position[0], position[2]);
+
+		vector position = editor_object.Position + terrainbuilder_offset;
+		vector orientation = editor_object.Orientation;
+		string line = string.Format("\"%1\";%2;%3;%4;%5;%6;%7;%8;", GetGame().GetModelName(editor_object.Type), position[0], position[2], orientation[0], orientation[1], orientation[2], editor_object.Scale, position[1]);
+		FPrintln(handle, line);
+	}
+	
+	return FileDialogResult.SUCCESS;
+}
