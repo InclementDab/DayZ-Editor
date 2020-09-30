aea46dd3c17915422682e8e22102190e590353ac
InclementDeath <tylerpaul2329@gmail.com>
2020-09-30 05:02:36 -0400

    Basic Prefab Binding

--- DayZEditor/Scripts/3_game/DayZEditor/gui/Dialogs/Prefabs/SliderPrefab.c
+++ DayZEditor/Scripts/3_game/DayZEditor/gui/Dialogs/Prefabs/SliderPrefab.c
@@ -6,15 +6,19 @@ class SliderPrefabController: PrefabBaseController
 class SliderPrefab: PrefabBase
 {
 	protected SliderPrefabController m_SliderPrefabController;
+	protected int m_Min, m_Max;
 	
 	void SliderPrefab(Widget parent = null, string caption = "", Controller binding_context = null, string binding_name = "", int min = 0, int max = 100)
 	{
+		m_Min = min; m_Max = max;
 		m_SliderPrefabController = SliderPrefabController.Cast(GetController());
+		EnScript.GetClassVar(m_BindingContext, m_BindingName, 0, m_SliderPrefabController.Value * m_Max);
+		m_SliderPrefabController.NotifyPropertyChanged("Value");
 	}
 	
 	override void PrefabPropertyChanged(string property_name)
 	{
-		EnScript.SetClassVar(m_BindingContext, m_BindingName, 0, m_SliderPrefabController.Value);
+		EnScript.SetClassVar(m_BindingContext, m_BindingName, 0, m_SliderPrefabController.Value * m_Max);
 		m_BindingContext.PropertyChanged(m_BindingName);
 	}
 	
--- DayZEditor/Scripts/4_world/DayZEditor/gui/Dialogs/EditorEnvironmentDialog.c
+++ DayZEditor/Scripts/4_world/DayZEditor/gui/Dialogs/EditorEnvironmentDialog.c
@@ -1,23 +1,41 @@
 
 class EditorEnvironmentDialogController: DialogBaseController
 {
+	protected World m_World;
 	
-	float TimeValue;
+	float Time;
 		
+	void EditorEnvironmentDialogController()
+	{
+		m_World = GetGame().GetWorld();
+		
+		int year, month, day, hour, minute;
+		m_World.GetDate(year, month, day, hour, minute);
+		Time = minute * 60 + hour * 3600;
+	}
+	
 	override void PropertyChanged(string property_name)
 	{
-	
+		switch (property_name) {
+			
+			case "Time": {
+				int year, month, day, hour, minute;
+				m_World.GetDate(year, month, day, hour, minute);
+				hour = Math.Floor(Time / 3600); 
+				minute = Time / 60 - hour * 60;
+				m_World.SetDate(year, month, day, hour, minute);
+				break;
+			}
+			
+		}
 	}
 }
 
 class EditorEnvironmentDialog: EditorDialogBase
-{
-	ref SliderPrefab TimeSlider;
-	
+{	
 	void EditorEnvironmentDialog(Widget parent = null, string title = "")
 	{
-		TimeSlider = new SliderPrefab(null, "Time", m_Controller, "TimeValue");
-		AddContent(TimeSlider);
+		AddContent(new SliderPrefab(null, "Time", m_Controller, "Time", 0, 86400));
 
 		AddButton(DialogResult.OK);
 	}
