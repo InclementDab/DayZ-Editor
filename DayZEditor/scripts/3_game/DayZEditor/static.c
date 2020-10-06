e33350ec2bcc8f32277a2a9e6a3b95ee07be6d6c
InclementDeath <tylerpaul2329@gmail.com>
2020-10-06 17:47:18 -0400

    Update static.c

--- DayZEditor/scripts/3_game/DayZEditor/static.c
+++ DayZEditor/scripts/3_game/DayZEditor/static.c
@@ -1,5 +1,16 @@
 #define EDITOR_PRINT
 
+// Enum for LogLevel. Utility for plugins and loggers
+enum LogLevel {
+	TRACE, 		// Logs at beginning of each method
+	DEBUG,		// Debug info within conditionals etc...
+	INFO,		// Helpful info within conditionals etc... (likely more user friendly)
+	WARNING,	// Warnings back to the user. unlikely to be mission critical
+	ERROR,		// Fatal errors. Mission critical
+	NONE
+};
+
+
 class EditorLog
 {
 	static LogLevel CurrentLogLevel = LogLevel.DEBUG;
