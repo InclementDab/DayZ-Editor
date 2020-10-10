ef108c2471b4f53fd60ffcf2a98bd9acbe928a44
InclementDeath <tylerpaul2329@gmail.com>
2020-10-10 18:03:15 -0400

    Terrain Builder exports now use relative height

--- DayZEditor/Scripts/4_world/DayZEditor/classes/FileManagers/EditorTerrainBuilderFile.c
+++ DayZEditor/Scripts/4_world/DayZEditor/classes/FileManagers/EditorTerrainBuilderFile.c
@@ -12,13 +12,13 @@ class EditorTerrainBuilderFile: EditorFileType
 		vector terrainbuilder_offset = Vector(200000, 0, 0);
 		foreach (EditorObjectData editor_object: data.EditorObjects) {
 			// "construction_house2";206638.935547;6076.024414;146.000015;0.000000;0.000000;1.000000;
-			// Name, X, Y, Yaw, Pitch, Roll, Scale, Relative Height										
-			//if (height_type == HeightType.RELATIVE)
-			//position[1] = position[1] - GetGame().SurfaceY(position[0], position[2]);
-	
+			// Name, X, Y, Yaw, Pitch, Roll, Scale, Relative Height			
 			vector position = editor_object.Position + terrainbuilder_offset;
+			
+			//if (height_type == HeightType.RELATIVE)
+			position[1] = position[1] - GetGame().SurfaceY(position[0], position[2]);
 			vector orientation = editor_object.Orientation;
-			string line = string.Format("\"%1\";%2;%3;%4;%5;%6;%7;%8;", GetGame().GetModelName(editor_object.Type), position[0], position[2], orientation[0], orientation[1], orientation[2], editor_object.GetScale(), position[1]);
+			string line = string.Format("\"%1\";%2;%3;%4;%5;%6;%7;%8;", GetGame().GetModelName(editor_object.Type), position[0], position[2], orientation[0], orientation[1], orientation[2], editor_object.Scale, position[1]);
 			FPrintln(handle, line);
 		}
 		
