9cd8df6b1cfd70d0fa7b4a4a139efb146c5e5ed7
InclementDeath <tylerpaul2329@gmail.com>
2020-10-11 19:03:37 -0400

    Fixed bug that would crash game upon pasting several times

--- DayZEditor/Scripts/4_world/DayZEditor/classes/Editor.c
+++ DayZEditor/Scripts/4_world/DayZEditor/classes/Editor.c
@@ -73,7 +73,7 @@ class Editor
 	bool 										SnappingMode;
 	bool 										CollisionMode;
 	
-	string										Version = "DayZ Editor Beta 1.0.29"; 
+	string										Version = "DayZ Editor Beta 1.0.291"; 
 
 	private void Editor(PlayerBase player) 
 	{
--- DayZEditor/Scripts/4_world/DayZEditor/classes/EditorClipboard.c
+++ DayZEditor/Scripts/4_world/DayZEditor/classes/EditorClipboard.c
@@ -69,7 +69,7 @@ class EditorClipboard
 				position
 			};
 						
-			EditorObject editor_object = GetEditor().CreateObject(pasted_object);
+			EditorObject editor_object = GetEditor().CreateObject(EditorObjectData.Create(pasted_object.Type, pasted_object.Position, pasted_object.Orientation, pasted_object.Flags));
 			float surfacey = GetGame().SurfaceY(position[0], position[2]);
 			vector size = editor_object.GetSize();
 			position[1] = surfacey + size[1] / 2;
