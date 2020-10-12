1f37e515476155bac149a447fc17212ec873974e
InclementDeath <tylerpaul2329@gmail.com>
2020-10-12 18:54:22 -0400

    Update Editor.c

--- DayZEditor/Scripts/4_world/DayZEditor/classes/Editor.c
+++ DayZEditor/Scripts/4_world/DayZEditor/classes/Editor.c
@@ -79,7 +79,7 @@ class Editor
 	bool 										SnappingMode;
 	bool 										CollisionMode;
 	
-	string										Version = "DayZ Editor Beta 1.0.292"; 
+	string										Version = "DayZ Editor Beta 1.0.293"; 
 
 	private void Editor(PlayerBase player) 
 	{
