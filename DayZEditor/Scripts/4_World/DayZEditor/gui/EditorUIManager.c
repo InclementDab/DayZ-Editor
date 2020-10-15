8a2e72b7405eaf4c4db53bb37e0c316a9c33a768
InclementDeath <tylerpaul2329@gmail.com>
2020-10-15 04:07:29 -0400

    Update EditorUIManager.c

--- DayZEditor/Scripts/4_World/DayZEditor/gui/EditorUIManager.c
+++ DayZEditor/Scripts/4_World/DayZEditor/gui/EditorUIManager.c
@@ -1,15 +1,12 @@
 class EditorUIManager
-{
-	
-	static float DialogLastX = -1;
-	static float DialogLastY = -1;
-	
+{	
 	// Modal Menu Control
 	static ref EditorMenu CurrentMenu;
 	
 	// ToolTip Control
 	private static ref ScriptView CurrentTooltip;
-	static void SetCurrentTooltip(ScriptView current_tooltip) {
+	static void SetCurrentTooltip(ScriptView current_tooltip) 
+	{
 		if (CurrentTooltip) {
 			delete CurrentTooltip;
 		}
@@ -23,6 +20,9 @@ class EditorUIManager
 	static bool IsDialogCommand(Widget w) {
 		return (CurrentDialog && CurrentDialog.GetLayoutRoot() && CurrentDialog.GetLayoutRoot().FindAnyWidget(w.GetName()));
 	}
+	
+	static float DialogLastX = -1;
+	static float DialogLastY = -1;
 	
 	static ref EditorHudController CurrentEditorHudController;
 	static ref EditorHudToolbarController CurrentEditorHudToolbarController;
