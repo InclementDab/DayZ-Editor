7a5bcbefaf1fc9375a709fa924cf1ae81a241269
InclementDeath <tylerpaul2329@gmail.com>
2020-10-31 19:01:41 -0400

    Update Deploy-Experimental.bat

--- Deploy-Experimental.bat
+++ Deploy-Experimental.bat
@@ -4,4 +4,5 @@ echo Deploying to Experimental. Are you sure?
 pause
 
 start /W "Build PBO" "%CD%/DayZEditor/Workbench/Batchfiles/ZBinarizeDeploy.bat"
+echo Uploading to Workshop
 start /W "Upload to Workshop" "%CD%/DayZEditor/Workbench/Deployment/Deployment.exe" experimental
