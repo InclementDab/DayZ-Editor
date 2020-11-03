8a191fbabaa461e9eb74fc00e430a5b19f328d4d
InclementDeath <tylerpaul2329@gmail.com>
2020-11-03 05:42:34 -0500

    optimized dialog

--- DayZEditor/Scripts/4_World/DayZEditor/gui/Dialogs/EditorPreferencesDialog.c
+++ DayZEditor/Scripts/4_World/DayZEditor/gui/Dialogs/EditorPreferencesDialog.c
@@ -3,30 +3,30 @@ class EditorPreferencesDialog: EditorDialogBase
 {	
 	void EditorPreferencesDialog(string title)
 	{
-		GroupPrefab general_group = new GroupPrefab("General", GetEditor().Settings, string.Empty);
+		GroupPrefab general_group = new GroupPrefab("General", m_Editor.Settings, string.Empty);
 		
 		DropdownListPrefabItem trace_item = new DropdownListPrefabItem("Trace", new Param1<LogLevel>(LogLevel.TRACE));
-		DropdownListPrefab log_level = new DropdownListPrefab("Log Level", GetEditor().Settings, "SelectedLogLevel", trace_item);
+		DropdownListPrefab log_level = new DropdownListPrefab("Log Level", m_Editor.Settings, "SelectedLogLevel", trace_item);
 		log_level.InsertItem(trace_item);
 		log_level.InsertItem("Debug", new Param1<LogLevel>(LogLevel.DEBUG));
 		log_level.InsertItem("Info", new Param1<LogLevel>(LogLevel.INFO));
 		log_level.InsertItem("Warning", new Param1<LogLevel>(LogLevel.WARNING));
 		log_level.InsertItem("Error", new Param1<LogLevel>(LogLevel.ERROR));
 		general_group.Insert(log_level);		
-		general_group.Insert(new EditBoxNumberPrefab("Auto Save", GetEditor().Settings, "AutoSaveTimer", GetEditor().Settings.AutoSaveTimer.ToString()));
+		general_group.Insert(new EditBoxNumberPrefab("Auto Save", m_Editor.Settings, "AutoSaveTimer", m_Editor.Settings.AutoSaveTimer.ToString()));
 		
 		
-		GroupPrefab game_group = new GroupPrefab("Game", GetEditor().Settings, string.Empty);
-		game_group.Insert(new SliderPrefab("View Distance", GetEditor().Settings, "ViewDistance", GetEditor().Settings.ViewDistance, 0, 20000));
-		game_group.Insert(new SliderPrefab("Object View Distance", GetEditor().Settings, "ObjectViewDistance", GetEditor().Settings.ObjectViewDistance, 0, 8000));
+		GroupPrefab game_group = new GroupPrefab("Game", m_Editor.Settings, string.Empty);
+		game_group.Insert(new SliderPrefab("View Distance", m_Editor.Settings, "ViewDistance", m_Editor.Settings.ViewDistance, 0, 20000));
+		game_group.Insert(new SliderPrefab("Object View Distance", m_Editor.Settings, "ObjectViewDistance", m_Editor.Settings.ObjectViewDistance, 0, 8000));
 		
-		GroupPrefab debug_group = new GroupPrefab("Editor", GetEditor().Settings, string.Empty);
-		debug_group.Insert(new EditBoxPrefab("Loot Spawns File", GetEditor().Settings, "EditorProtoFile", GetEditor().Settings.EditorProtoFile));
-		debug_group.Insert(new EditBoxPrefab("Brush File", GetEditor().Settings, "EditorBrushFile", GetEditor().Settings.EditorBrushFile));
-		debug_group.Insert(new CheckBoxPrefab("Modal Dialogs", GetEditor().Settings, "LockCameraDuringDialogs", GetEditor().Settings.LockCameraDuringDialogs));
-		debug_group.Insert(new CheckBoxPrefab("Show Bounding Boxes", GetEditor().Settings, "ShowBoundingBoxes", GetEditor().Settings.ShowBoundingBoxes));
-		debug_group.Insert(new CheckBoxPrefab("Brushed Object Markers", GetEditor().Settings, "BrushedObjectMarkers", GetEditor().Settings.BrushedObjectMarkers));
-		debug_group.Insert(new CheckBoxPrefab("Debug Mode", GetEditor().Settings, "DebugMode", GetEditor().Settings.DebugMode));
+		GroupPrefab debug_group = new GroupPrefab("Editor", m_Editor.Settings, string.Empty);
+		debug_group.Insert(new EditBoxPrefab("Loot Spawns File", m_Editor.Settings, "EditorProtoFile"));
+		debug_group.Insert(new EditBoxPrefab("Brush File", m_Editor.Settings, "EditorBrushFile"));
+		debug_group.Insert(new CheckBoxPrefab("Modal Dialogs", m_Editor.Settings, "LockCameraDuringDialogs"));
+		debug_group.Insert(new CheckBoxPrefab("Show Bounding Boxes", m_Editor.Settings, "ShowBoundingBoxes"));
+		debug_group.Insert(new CheckBoxPrefab("Brushed Object Markers", m_Editor.Settings, "BrushedObjectMarkers"));
+		debug_group.Insert(new CheckBoxPrefab("Debug Mode", m_Editor.Settings, "DebugMode", m_Editor.Settings.DebugMode));
 		
 		AddContent(general_group);
 		AddContent(game_group);
