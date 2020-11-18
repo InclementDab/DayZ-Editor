bedcdec40f6c4edf8d5992d8a8f3b4bc8bbab5bb
InclementDeath <tylerpaul2329@gmail.com>
2020-11-18 06:26:27 -0500

    Added support for object deletion in init.c file

--- DayZEditor/Scripts/Data/Defaults/init.c
+++ DayZEditor/Scripts/Data/Defaults/init.c
@@ -10,7 +10,19 @@ static void SpawnObject(string type, vector position, vector orientation)
 	if (obj.CanAffectPathgraph()) GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(GetGame().UpdatePathgraphRegionByObject, 100, false, obj);
 }
 
+static ref map<int, Object> WorldObjects;
 static void DeleteObject(int object_id)
 {
-    // todo
+    // Adds all map objects to the WorldObjects array
+    if (!WorldObjects) {
+        WorldObjects = new map<int, Object>();
+		ref array<Object> objects = {};
+		ref array<CargoBase> cargos = {};
+		GetGame().GetObjectsAtPosition(Vector(7500, 0, 7500), 20000, objects, cargos);
+		foreach (Object o: objects) {
+			WorldObjects.Insert(o.GetID(), o);
+		}
+    }
+
+    CF_ObjectManager.HideMapObject(WorldObjects[object_id]);
 }
