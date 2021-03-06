8b01b47dc0fe26bb0dad36476bf88dfd167e7dd8
Tyler Paul <tylerpaul2329@gmail.com>
2021-03-06 13:19:33 -0500

    Removed old cache method, fixed some bugs

--- DayZEditor/Scripts/4_World/DayZEditor/classes/Editor.c
+++ DayZEditor/Scripts/4_World/DayZEditor/classes/Editor.c
@@ -1021,7 +1021,6 @@ class Editor
 		GetGame().GetObjectsAtPosition3D(position, radius, objects, cargos);
 		
 		foreach (Object building: objects) {
-			Print(building.GetType());
 			if (building.GetType() == name) {
 				return building;
 			}
