86df0efdb81ef91e31278c8f955ac50cf0269c77
Tyler Paul <tylerpaul2329@gmail.com>
2021-05-07 02:48:09 -0400

    Update EditorDragHandler.c

--- DayZEditor/Scripts/4_World/DayZEditor/classes/EditorDragHandler.c
+++ DayZEditor/Scripts/4_World/DayZEditor/classes/EditorDragHandler.c
@@ -320,16 +320,9 @@ class MapDragHandler: DragHandler
 		if (GetEditor().GroundMode) {
 			vector target_pos = target.GetPosition();
 			transform[3][1] = GetGame().SurfaceY(pos[0], pos[2]) + target_pos[1] - GetGame().SurfaceY(target_pos[0], target_pos[2]);
-			
 		} else {
 			transform[3][1] = GetGame().SurfaceY(pos[0], pos[2]) + target.GetSize()[1] / 2;
 		}
-		
-		
-		
-		
-		
-		
 	}
 }
 
