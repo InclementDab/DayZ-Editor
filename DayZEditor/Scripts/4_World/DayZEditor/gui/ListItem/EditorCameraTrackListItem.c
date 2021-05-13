869e1d3a1b22d1f1c1207004e5eccc23a1cca86f
Tyler Paul <tylerpaul2329@gmail.com>
2021-05-13 00:30:20 -0400

    Proper Parsing and toggling of Panel

--- DayZEditor/Scripts/4_World/DayZEditor/gui/ListItem/EditorCameraTrackListItem.c
+++ DayZEditor/Scripts/4_World/DayZEditor/gui/ListItem/EditorCameraTrackListItem.c
@@ -1,10 +1,17 @@
 class EditorCameraTrackListItemController: Controller
-{
+{	
+	vector Position0;
+	vector Position1;
+	
+	
 	string Name;
 	StringEvaluater pX, pY, pZ;
 	StringEvaluater oX, oY, oZ;
 	float Time;
 	bool FlipOrientation;
+	
+	bool CollapseTab = false;
+	Widget CollapsePanel;
 	
 	vector GetPosition()
 	{
@@ -14,6 +21,16 @@ class EditorCameraTrackListItemController: Controller
 	vector GetOrientation()
 	{
 		return Vector(oX.Parse(), oY.Parse(), oZ.Parse());
+	}
+	
+	override void PropertyChanged(string property_name)
+	{
+		switch (property_name) {
+			case "CollapseTab": {
+				CollapsePanel.Show(CollapseTab);
+				break;
+			}
+		}
 	}
 }
 
@@ -37,24 +54,22 @@ class EditorCameraTrackListItem: ScriptViewTemplate<EditorCameraTrackListItemCon
 	
 	void OnFlyToExecute(ButtonCommandArgs args)
 	{
-		EditorCameraTrackListItemController template_controller = GetTemplateController();
-		GetEditor().GetCamera().SetPosition(template_controller.GetPosition());
-		GetEditor().GetCamera().SetOrientation(template_controller.GetOrientation());
+		GetEditor().GetCamera().SetPosition(m_TemplateController.GetPosition());
+		GetEditor().GetCamera().SetOrientation(m_TemplateController.GetOrientation());
 	}
 	
 	void SetData(vector position, vector orientation, float time)
 	{
-		EditorCameraTrackListItemController template_controller = GetTemplateController();
-		template_controller.pX = position[0].ToString();
-		template_controller.pY = position[1].ToString();
-		template_controller.pZ = position[2].ToString();
-		template_controller.oX = orientation[0].ToString();
-		template_controller.oY = orientation[1].ToString();
-		template_controller.oZ = orientation[2].ToString();
-		template_controller.Time = time;
-		template_controller.NotifyPropertyChanged();
+		m_TemplateController.pX = position[0].ToString();
+		m_TemplateController.pY = position[1].ToString();
+		m_TemplateController.pZ = position[2].ToString();
+		m_TemplateController.oX = orientation[0].ToString();
+		m_TemplateController.oY = orientation[1].ToString();
+		m_TemplateController.oZ = orientation[2].ToString();
+		m_TemplateController.Time = time;
+		m_TemplateController.NotifyPropertyChanged();
 	}
-	
+		
 	EditorCameraTrackListItemController GetData()
 	{
 		return GetTemplateController();
