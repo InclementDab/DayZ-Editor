a1151f7f4d6335bc99759b6dcc10972b33b896dc
Tyler Paul <tylerpaul2329@gmail.com>
2021-05-20 20:00:51 -0400

    Fixed New command not doing anything

--- DayZEditor/Scripts/4_World/DayZEditor/classes/Commands/Hud/EditorNewCommand.c
+++ DayZEditor/Scripts/4_World/DayZEditor/classes/Commands/Hud/EditorNewCommand.c
@@ -3,9 +3,25 @@ class EditorNewCommand: EditorCommand
 	protected override void Call(Class sender, CommandArgs args) 
 	{
 		EditorEditBoxDialog edit_dialog = new EditorEditBoxDialog("New File...");
-		string edit_data;
-		DialogResult result = edit_dialog.ShowDialog(edit_data);
-		if (result != DialogResult.OK) return;
+		string file_name;
+		DialogResult result = edit_dialog.ShowDialog(file_name);
+		if (result != DialogResult.OK) { 
+			return;
+		}
+		
+		EditorFileManager.GetSafeFileName(file_name, ".dze");
+		
+		// Only supporting new in root dir atm
+		if (FileExist("$profile:Editor/" + file_name)) {
+			if (MessageBox.Show("Are you sure?", "File " + file_name + " already exists. Overwrite?", MessageBoxButtons.OKCancel) == DialogResult.Cancel) {
+				return;
+			}
+		}
+		
+		m_Editor.Clear();
+		m_Editor.SetSaveFile(file_name);
+		
+		GetEditor().GetEditorHud().CreateNotification("New File Created", COLOR_GREEN);
 	}
 		
 	override string GetName() 
