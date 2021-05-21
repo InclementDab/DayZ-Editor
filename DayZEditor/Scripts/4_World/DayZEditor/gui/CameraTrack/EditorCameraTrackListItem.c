170eae52801452e8ac1bb366b61e6dc446137e88
Tyler Paul <tylerpaul2329@gmail.com>
2021-05-21 05:25:53 -0400

    Update EditorCameraTrackListItem.c

--- DayZEditor/Scripts/4_World/DayZEditor/gui/CameraTrack/EditorCameraTrackListItem.c
+++ DayZEditor/Scripts/4_World/DayZEditor/gui/CameraTrack/EditorCameraTrackListItem.c
@@ -1,28 +1,78 @@
-
 class EditorCameraTrackListItemController: Controller
-{
-	float pX, pY, pZ;
-	float oX, oY, oZ;
+{	
+	string Name;
+	StringEvaluater pX, pY, pZ;
+	StringEvaluater oX, oY, oZ;
 	float Time;
+	bool FlipOrientation;
+	
+	bool CollapseTab = false;
+	Widget CollapsePanel;
+	
+	vector GetPosition()
+	{
+		return Vector(pX.Parse(), pY.Parse(), pZ.Parse());
+	}
+	
+	vector GetOrientation()
+	{
+		return Vector(oX.Parse(), oY.Parse(), oZ.Parse());
+	}
+	
+	override void PropertyChanged(string property_name)
+	{
+		switch (property_name) {
+			case "CollapseTab": {
+				CollapsePanel.Show(CollapseTab);
+				break;
+			}
+		}
+	}
 }
 
 class EditorCameraTrackListItem: ScriptViewTemplate<EditorCameraTrackListItemController>
 {
-	void EditorCameraTrackListItem(vector position, vector orientation, float time)
+	void EditorCameraTrackListItem(vector position, vector orientation, float time, string name)
 	{
-		EditorCameraTrackListItemController template_controller = GetTemplateController();
-		template_controller.pX = position[0];
-		template_controller.pY = position[1];
-		template_controller.pZ = position[2];
-		template_controller.oX = orientation[0];
-		template_controller.oY = orientation[1];
-		template_controller.oZ = orientation[2];
-		template_controller.Time = time;
-		template_controller.NotifyPropertyChanged();
+		GetTemplateController().Name = name;
+		SetData(position, orientation, time);
+	}
+	
+	void OnSetExecute(ButtonCommandArgs args)
+	{
+		SetData(GetEditor().GetCamera().GetPosition(), GetEditor().GetCamera().GetOrientation(), GetTemplateController().Time);
+	}
+	
+	void OnDeleteExecute(ButtonCommandArgs args)
+	{
+		delete this;
+	}
+	
+	void OnFlyToExecute(ButtonCommandArgs args)
+	{
+		GetEditor().GetCamera().SetPosition(m_TemplateController.GetPosition());
+		GetEditor().GetCamera().SetOrientation(m_TemplateController.GetOrientation());
+	}
+	
+	void SetData(vector position, vector orientation, float time)
+	{
+		m_TemplateController.pX = position[0].ToString();
+		m_TemplateController.pY = position[1].ToString();
+		m_TemplateController.pZ = position[2].ToString();
+		m_TemplateController.oX = orientation[0].ToString();
+		m_TemplateController.oY = orientation[1].ToString();
+		m_TemplateController.oZ = orientation[2].ToString();
+		m_TemplateController.Time = time;
+		m_TemplateController.NotifyPropertyChanged();
+	}
+		
+	EditorCameraTrackListItemController GetData()
+	{
+		return GetTemplateController();
 	}
 	
 	override string GetLayoutFile()
 	{
-		return "DayZEditor/GUI/layouts/cameratrack/CameraTrackListItem.layout";
+		return "DayZEditor/GUI/layouts/cameratrack/CameraTrackGroup.layout";
 	}
 }
