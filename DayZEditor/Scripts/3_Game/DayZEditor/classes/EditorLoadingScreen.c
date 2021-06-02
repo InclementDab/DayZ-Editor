7962b68db77804cd308634d85894bb8bf1f7a9c3
Tyler Paul <tylerpaul2329@gmail.com>
2021-06-02 02:04:38 -0400

    veryPog

--- DayZEditor/Scripts/3_Game/DayZEditor/classes/EditorLoadingScreen.c
+++ DayZEditor/Scripts/3_Game/DayZEditor/classes/EditorLoadingScreen.c
@@ -1,6 +1,6 @@
 modded class LoadingScreen
 {
-	static const int LOADING_SCREEN_COUNT = 13;
+	static const int LOADING_SCREEN_COUNT = 14;
 	
 	void LoadingScreen(DayZGame game)
 	{		
@@ -27,7 +27,6 @@ modded class LoadingScreen
 		m_ImageLogoCorner.SetColor(ARGB(220, 255, 255, 255));
 		
 		m_ImageWidgetBackground.SetFlags(WidgetFlags.SOURCEALPHA | WidgetFlags.BLEND);
-		//m_EditorLogo = game.GetLoadingWorkspace().CreateWidgets("DayZEditor/gui/layouts/EditorLogo.layout", m_ImageWidgetBackground);
 		
 		int s_x, s_y;
 		GetScreenSize(s_x, s_y);
@@ -43,7 +42,7 @@ modded class LoadingScreen
 		val = val + (val << 37);
 		val = Math.AbsInt(val);
 		val = val % LOADING_SCREEN_COUNT + 1;
-		m_ImageWidgetBackground.LoadImageFile(0, string.Format("DayZEditor/gui/loadingscreens/%1.edds", val));
+		m_ImageWidgetBackground.LoadImageFile(0, string.Format("DayZEditor/gui/loadingscreens/%1.edds", 14));
 		m_ImageWidgetBackground.SetImage(0);
 	}
 }
