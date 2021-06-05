80829bcb9109935d0f558a575c84af3659aad2de
Tyler Paul <tylerpaul2329@gmail.com>
2021-06-05 01:12:32 -0400

    Refactored brushes to leverage multi-object deletions

--- DayZEditor/Scripts/4_World/DayZEditor/classes/Brushes/EditorDeleteBrush.c
+++ DayZEditor/Scripts/4_World/DayZEditor/classes/Brushes/EditorDeleteBrush.c
@@ -12,16 +12,25 @@ class DeleteBrush: EditorBrush
 		//DayZPhysics.RaycastRV(position - surface_normal * 5, position + surface_normal * 500, contact_pos, contact_dir, component, results, null, null, false, false, 0, EditorBrush.GetRadius() / 2, CollisionFlags.ALLOBJECTS);
 		//GetEditor().ClearSelection();
 		
-		// todo this is deleting one at a time. use DeleteObjects smile :)
+		EditorObjectMap editor_objects();
+		EditorDeletedObjectMap deleted_objects();
 		foreach (Object r: objects) {
 			EditorObject eo = GetEditor().GetEditorObject(r);
 			if (eo) {
-				GetEditor().DeleteObject(eo);
+				editor_objects.InsertEditorObject(eo);
 			} else {
 				if (!r.IsKindOf("BrushBase")) {
-					GetEditor().HideMapObject(r);
+					deleted_objects.InsertEditorDeletedObject(new EditorDeletedObject(r));
 				}
 			}
-		}		
+		}	
+		
+		if (editor_objects.Count() > 0) {
+			GetEditor().DeleteObjects(editor_objects);
+		}
+		
+		if (deleted_objects.Count() > 0) {
+			GetEditor().HideMapObjects(deleted_objects);
+		}
 	}
 }
