48d391eec845d03fca9fcaca0a5ee1b7cd212c13
Tyler Paul <tylerpaul2329@gmail.com>
2021-06-04 23:54:58 -0400

    names that are too long will get downsized

--- DayZEditor/Scripts/4_World/DayZEditor/gui/ListItem/EditorDeletedListItem.c
+++ DayZEditor/Scripts/4_World/DayZEditor/gui/ListItem/EditorDeletedListItem.c
@@ -6,7 +6,12 @@ class EditorDeletedListItem: EditorListItem
 	{
 		m_EditorDeletedObject = deleted_object;
 		
-		m_TemplateController.Label = string.Format("%1 (%2)", m_EditorDeletedObject.GetType(), m_EditorDeletedObject.GetID());
+		string item_name = m_EditorDeletedObject.GetType();
+		if (item_name.Length() >= 18) {
+			item_name = item_name.Substring(0, 15) + "...";
+		}
+		
+		m_TemplateController.Label = string.Format("%1 (%2)", item_name, m_EditorDeletedObject.GetID());
 		m_TemplateController.NotifyPropertyChanged("Label");
 		
 		//m_TemplateController.Icon = m_EditorDeletedObject.GetData().Icon;
