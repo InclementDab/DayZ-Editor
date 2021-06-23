9309a232793769a157d7ee99f1db5f5f2acd989e
Tyler Paul <tylerpaul2329@gmail.com>
2021-06-23 15:25:49 -0400

    Update LayoutBindingAddons.c

--- DayZEditor/Scripts/3_Game/DayZEditor/LayoutBindingAddons.c
+++ DayZEditor/Scripts/3_Game/DayZEditor/LayoutBindingAddons.c
@@ -106,7 +106,6 @@ modded class MVC
 	override void RegisterConversionTemplates(out TypeConversionHashMap type_conversions)
 	{
 		super.RegisterConversionTemplates(type_conversions);
-		type_conversions.Insert(StringEvaluater, TypeConversionEquation);
 		type_conversions.Insert(EditorWidget, TypeConversionEditorWidget);
 		type_conversions.Insert(EditorBrushData, TypeConversionBrush);
 		type_conversions.Insert(EditorBrushObject, TypeConversionBrushObject);
