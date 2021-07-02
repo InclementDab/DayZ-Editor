d908313d7caa98c61f8a7b284d4d116081f52922
Tyler Paul <tylerpaul2329@gmail.com>
2021-07-01 23:50:14 -0400

    Fixed a bug that would cause the Recent Files not to load properly (for some reason?)

--- DayZEditor/Scripts/4_World/DayZEditor/gui/Menus/Menus/EditorOpenRecentMenu.c
+++ DayZEditor/Scripts/4_World/DayZEditor/gui/Menus/Menus/EditorOpenRecentMenu.c
@@ -4,11 +4,11 @@ class EditorOpenRecentMenu: EditorMenu
 	{
 		EditorLog.Trace("EditorOpenRecentMenu");
 		
-		array<string> recent_files = GetEditor().GetRecentFiles();
+		TStringArray recent_files = GetEditor().GetRecentFiles();
 				
 		// its backwards
 		for (int i = recent_files.Count() - 1; i >= 0; i--) {
-			AddMenuItem(new EditorOpenRecentListItem(recent_files[i]));
+			AddMenuItem(new EditorOpenRecentListItem(String(recent_files[i])));
 		}
 	}
 }
