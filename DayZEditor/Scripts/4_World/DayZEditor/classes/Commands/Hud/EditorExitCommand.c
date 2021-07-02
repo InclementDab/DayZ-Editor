993deac1c48226034a9e2f169f975838fdb837be
Tyler Paul <tylerpaul2329@gmail.com>
2021-07-02 11:49:45 -0400

    Fixed bug that would cause the Editor to crash when exiting after placing several objects

--- DayZEditor/Scripts/4_World/DayZEditor/classes/Commands/Hud/EditorExitCommand.c
+++ DayZEditor/Scripts/4_World/DayZEditor/classes/Commands/Hud/EditorExitCommand.c
@@ -3,7 +3,13 @@ class EditorExitCommand: EditorCommand
 	protected override void Call(Class sender, CommandArgs args) 
 	{
 		DialogResult result = EditorMessageBox.Show("Exit to main menu", "Do you want to close the DayZ Editor?", MessageBoxButtons.OKCancel);
-		if (result != DialogResult.OK) return;
+		if (result != DialogResult.OK) { 
+			return;
+		}
+		
+		if (m_Editor) {
+			m_Editor.Clear();
+		}
 		
 		GetGame().AbortMission();
 	}
