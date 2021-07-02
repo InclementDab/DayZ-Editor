d8d348a56b1a62f4b2f126a3af2b103850c47f2b
Tyler Paul <tylerpaul2329@gmail.com>
2021-07-02 15:59:39 -0400

    Fixed Delete Brush being able to delete bounding boxes

--- DayZEditor/Scripts/4_World/DayZEditor/classes/Brushes/EditorDeleteBrush.c
+++ DayZEditor/Scripts/4_World/DayZEditor/classes/Brushes/EditorDeleteBrush.c
@@ -19,7 +19,7 @@ class DeleteBrush: EditorBrush
 			if (eo) {
 				editor_objects.InsertEditorObject(eo);
 			} else {
-				if (!r.IsKindOf("BrushBase")) {
+				if (!r.IsKindOf("BrushBase") && !r.IsKindOf("BoundingBoxBase")) {
 					deleted_objects.InsertEditorDeletedObject(new EditorDeletedObject(r));
 				}
 			}
