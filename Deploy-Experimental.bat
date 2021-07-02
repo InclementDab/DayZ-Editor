6e0a2cb5d849da11a6606cfdd037ce26fd93b009
Tyler Paul <tylerpaul2329@gmail.com>
2021-07-02 11:17:01 -0400

    removed increment from deploy

--- Deploy-Experimental.bat
+++ Deploy-Experimental.bat
@@ -2,18 +2,6 @@
 
 echo Deploying to Experimental. Are you sure?
 
-set c=%CD%
-
-cd DayZEditor/Scripts/Data
-
-if not exist build.txt >build.txt echo 0
-for /f %%x in (build.txt) do (
-set /a var=%%x+1
-)
->build.txt echo %var%
-
-cd %c%
-
 start /W "Build PBO" "%CD%/DayZEditor/Workbench/Batchfiles/ZBinarizeDeploy.bat"
 echo Uploading to Workshop
 cd "%CD%/DayZEditor/Workbench/Deployment"
