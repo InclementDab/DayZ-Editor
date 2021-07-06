006b51e260b366ab481ecefbf2f2cf8be5c850eb
Tyler Paul <tylerpaul2329@gmail.com>
2021-07-06 15:22:40 -0400

    Localized Object Properties

--- DayZEditor/Scripts/4_World/DayZEditor/gui/Dialogs/EditorObjectPropertiesDialog.c
+++ DayZEditor/Scripts/4_World/DayZEditor/gui/Dialogs/EditorObjectPropertiesDialog.c
@@ -44,22 +44,22 @@ class EditorObjectPropertiesDialog: EditorDialogBase
 		
 		UpdateViewContext();
 		
-		m_GeneralGroup = new GroupPrefab("General", m_EditorObject, string.Empty);
-		m_GeneralGroup.Insert(new CheckBoxPrefab("Show", m_EditorObject, "Show"));
-		m_GeneralGroup.Insert(new EditBoxPrefab("Name", m_EditorObject, "Name"));
-		m_GeneralGroup.Insert(new VectorPrefab("Position", m_EditorObject, "Position"));
-		m_GeneralGroup.Insert(new VectorPrefab("Orientation", m_EditorObject, "Orientation"));
-		m_GeneralGroup.Insert(new EditBoxNumberPrefab("Scale", m_EditorObject, "Scale", 0.01));
-		//m_GeneralGroup.Insert(new CheckBoxPrefab("Editor Only", m_EditorObject, "EditorOnly", m_EditorObject.EditorOnly));
+		m_GeneralGroup = new GroupPrefab("#STR_EDITOR_GENERAL", m_EditorObject, string.Empty);
+		m_GeneralGroup.Insert(new CheckBoxPrefab("#STR_EDITOR_SHOW", m_EditorObject, "Show"));
+		m_GeneralGroup.Insert(new EditBoxPrefab("#STR_EDITOR_NAME", m_EditorObject, "Name"));
+		m_GeneralGroup.Insert(new VectorPrefab("#STR_EDITOR_POSITION", m_EditorObject, "Position"));
+		m_GeneralGroup.Insert(new VectorPrefab("#STR_EDITOR_ORIENTATION", m_EditorObject, "Orientation"));
+		m_GeneralGroup.Insert(new EditBoxNumberPrefab("#STR_EDITOR_SCALE", m_EditorObject, "Scale", 0.01));
+		//m_GeneralGroup.Insert(new CheckBoxPrefab("#STR_EDITOR_EDITOR_ONLY", m_EditorObject, "EditorOnly", m_EditorObject.EditorOnly));
 		m_GeneralGroup.Open(GeneralGroup);
 		
-		m_ObjectGroup = new GroupPrefab("Object Settings", m_EditorObject, string.Empty);
-		m_ObjectGroup.Insert(new EditBoxNumberPrefab("Health", m_EditorObject, "Health"));
-		m_ObjectGroup.Insert(new CheckBoxPrefab("Enable Simulation", m_EditorObject, "Simulate"));
-		m_ObjectGroup.Insert(new CheckBoxPrefab("Lock", m_EditorObject, "Locked"));
-		m_ObjectGroup.Insert(new CheckBoxPrefab("Enable Physics", m_EditorObject, "Physics"));
-		m_ObjectGroup.Insert(new CheckBoxPrefab("Allow Damage", m_EditorObject, "AllowDamage"));
-		//m_ObjectGroup.Insert(new CheckBoxPrefab("Enable Collision", m_EditorObject, "Collision"));
+		m_ObjectGroup = new GroupPrefab("#STR_EDITOR_OBJECT", m_EditorObject, string.Empty);
+		m_ObjectGroup.Insert(new EditBoxNumberPrefab("#STR_EDITOR_HEALTH", m_EditorObject, "Health"));
+		m_ObjectGroup.Insert(new CheckBoxPrefab("#STR_EDITOR_ENABLE_SIMULATION", m_EditorObject, "Simulate"));
+		m_ObjectGroup.Insert(new CheckBoxPrefab("#STR_EDITOR_LOCK", m_EditorObject, "Locked"));
+		m_ObjectGroup.Insert(new CheckBoxPrefab("#STR_EDITOR_ENABLE_PHYSICS", m_EditorObject, "Physics"));
+		m_ObjectGroup.Insert(new CheckBoxPrefab("#STR_EDITOR_ENABLE_DAMAGE", m_EditorObject, "AllowDamage"));
+		//m_ObjectGroup.Insert(new CheckBoxPrefab("#STR_EDITOR_ENABLE_COLLISION", m_EditorObject, "Collision"));
 		m_ObjectGroup.Open(Object_Group);
 		
 		m_AdvancedGroup = new GroupPrefab("Debug", m_EditorObject, string.Empty);
@@ -74,10 +74,10 @@ class EditorObjectPropertiesDialog: EditorDialogBase
 		AddContent(m_AdvancedGroup);
 		
 		if (editor_object.GetWorldObject().IsMan()) {
-			m_HumanGroup = new GroupPrefab("Human ViewController", m_EditorObject, string.Empty);
-			m_HumanGroup.Insert(new CheckBoxPrefab("Selected Player", m_EditorObject, "Control"));
-			m_HumanGroup.Insert(new EditBoxPrefab("Animation", m_EditorObject, "CurrentAnimation"));
-			m_HumanGroup.Insert(new ButtonPrefab("Animate", m_EditorObject, "Animate"));
+			m_HumanGroup = new GroupPrefab("#STR_EDITOR_HUMAN", m_EditorObject, string.Empty);
+			m_HumanGroup.Insert(new CheckBoxPrefab("#STR_EDITOR_SELECTED_PLAYER", m_EditorObject, "Control"));
+			m_HumanGroup.Insert(new EditBoxPrefab("#STR_EDITOR_ANIMATION", m_EditorObject, "CurrentAnimation"));
+			m_HumanGroup.Insert(new ButtonPrefab("#STR_EDITOR_CINEMATIC_CAMERA_RUN", m_EditorObject, "Animate"));
 			m_HumanGroup.Open(HumanGroup);
 			AddContent(m_HumanGroup);
 		}
