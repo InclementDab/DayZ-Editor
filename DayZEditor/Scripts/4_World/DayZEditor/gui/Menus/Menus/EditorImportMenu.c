2791aecaa10dbdb639c1dda4b4a565213763cdaa
Tyler Paul <tylerpaul2329@gmail.com>
2021-07-14 18:36:07 -0400

    Registered DZEBin commands

--- DayZEditor/Scripts/4_World/DayZEditor/gui/Menus/Menus/EditorImportMenu.c
+++ DayZEditor/Scripts/4_World/DayZEditor/gui/Menus/Menus/EditorImportMenu.c
@@ -9,5 +9,6 @@ class EditorImportMenu: EditorMenu
 		AddMenuButton(m_Editor.CommandManager[EditorImportFromTerrainBuilder]);
 		AddMenuButton(m_Editor.CommandManager[EditorImportFromVPP]);
 		AddMenuButton(m_Editor.CommandManager[EditorImportFromCOM]);
+		AddMenuButton(m_Editor.CommandManager[EditorImportFromDZEBin]);
 	}
 }
