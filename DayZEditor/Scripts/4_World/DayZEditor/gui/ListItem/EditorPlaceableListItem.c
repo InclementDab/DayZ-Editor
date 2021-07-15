d1ee2cc6a0f4817b005986913748e0838e512ac5
Tyler Paul <tylerpaul2329@gmail.com>
2021-07-15 07:21:30 -0400

    null ptr with layout change

--- DayZEditor/Scripts/4_World/DayZEditor/gui/ListItem/EditorPlaceableListItem.c
+++ DayZEditor/Scripts/4_World/DayZEditor/gui/ListItem/EditorPlaceableListItem.c
@@ -13,7 +13,6 @@ class EditorPlaceableListItem: EditorListItem
 	{
 		m_PlaceableItem = placeable_item;
 		
-		ListItemShow.Show(false);
 		m_TemplateController.Label = placeable_item.Type;
 		m_TemplateController.NotifyPropertyChanged("Label");
 		
