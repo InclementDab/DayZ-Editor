1cd11823f86b3d29f6ccf85ac541c9375b8ffddb
Tyler Paul <tylerpaul2329@gmail.com>
2021-08-11 23:52:14 -0400

    Added version 1 deserializing, moving to v2

--- BinEditor/Enfusion-Deserializer/Enfusion-Deserializer/Program.cs
+++ BinEditor/Enfusion-Deserializer/Enfusion-Deserializer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace Enfusion_Deserializer
 {
@@ -8,28 +10,21 @@ namespace Enfusion_Deserializer
     {
         static void Main(string[] args)
         {
+            EnfusionSerializer stream = new("P:\\profiles\\Client\\Users\\tyler\\Editor\\test.dze", FileMode.Open, FileAccess.Read);
             EditorSaveData data = new();
+            Console.WriteLine(data.Read(stream));
 
-            FileStream stream = new FileStream("P:\\profiles\\Client\\test.bin", FileMode.Open, FileAccess.Read);
-            // Console.WriteLine(stream.Length);
-
-
-            /*Console.WriteLine(EditorSaveData.ReadInt(stream));
-            Console.WriteLine(EditorSaveData.ReadInt(stream));
-            Console.WriteLine(EditorSaveData.ReadFloat(stream));
-            */
-
-            Console.WriteLine(EditorSaveData.ReadVector(stream));
+            Console.WriteLine(data.MapName);
 
             Console.ReadKey();
         }
     }
 
-    public class Vector
+    public class vector
     {
         public float X, Y, Z;
 
-        public Vector(float x = 0, float y = 0, float z = 0)
+        public vector(float x = 0, float y = 0, float z = 0)
         {
             X = x; Y = y; Z = z;
         }
@@ -54,7 +49,7 @@ namespace Enfusion_Deserializer
             }
         }
 
-        public static readonly Vector Zero = new();
+        public static readonly vector Zero = new();
 
         public override string ToString()
         {
@@ -64,36 +59,139 @@ namespace Enfusion_Deserializer
 
     public class EditorSaveData
     {
-        public int test;
+        public int Version;
+        public string MapName;
+        public vector CameraPosition;
 
-        public static int ReadInt(FileStream stream)
+        public List<EditorObjectData> EditorObjects = new();
+        public List<EditorDeletedObjectData> EditorDeletedObjects = new();
+
+        public bool Read(EnfusionSerializer stream)
+        {
+            string bin_check = stream.ReadString();
+            Console.WriteLine(bin_check);
+            if (bin_check != "EditorBinned") {
+                return false;
+            }
+
+            Version = stream.ReadInt();
+            MapName = stream.ReadString();
+            CameraPosition = stream.ReadVector();
+
+            int objects_length = stream.ReadInt();
+            Console.WriteLine("--Placements--");
+            for (int i = 0; i < objects_length; i++) {
+                EditorObjectData editor_object = new();
+                editor_object.Read(stream, Version);
+                Console.WriteLine(editor_object.ToString());
+                EditorObjects.Add(editor_object);
+            }
+
+            int deletions_length = stream.ReadInt();
+            Console.WriteLine("--Deletions--");
+            for (int i = 0; i < deletions_length; i++) {
+                EditorDeletedObjectData deletion_data = new();
+                deletion_data.Read(stream, Version);
+                Console.WriteLine(deletion_data.ToString());
+                EditorDeletedObjects.Add(deletion_data);
+            }
+
+            return true;
+        }
+    }
+
+    public class EditorObjectData
+    {
+        public string Type;
+        public string DisplayName;
+        public vector Position;
+        public vector Orientation;
+        public float Scale = 1;
+        public int Flags;
+
+        public bool Read(EnfusionSerializer stream, int version)
+        {
+            Type = stream.ReadString();
+            DisplayName = stream.ReadString();
+            Position = stream.ReadVector();
+            Orientation = stream.ReadVector();
+            Scale = stream.ReadFloat();
+            Flags = stream.ReadInt();
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"{Type}: <{DisplayName}> at {Position}, {Orientation}";
+        }
+    }
+
+    public class EditorDeletedObjectData
+    {
+        public string Type;
+        public vector Position;
+        public int Flags;
+
+        public bool Read(EnfusionSerializer stream, int version)
+        {
+            Type = stream.ReadString();
+            Position = stream.ReadVector();
+            Flags = stream.ReadInt();
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"{Type}: {Position}";
+        }
+    }
+
+    public class EnfusionSerializer : FileStream
+    {
+        public EnfusionSerializer(string path, FileMode mode, FileAccess access) : base(path, mode, access)
+        { }
+
+        public int ReadInt()
         {
             byte[] bytes = new byte[4];
-            stream.Read(bytes, 0, 4);
+            Read(bytes, 0, 4);
             return BitConverter.ToInt32(bytes);
         }
 
-        public static float ReadFloat(FileStream stream)
+        public float ReadFloat()
         {
             byte[] bytes = new byte[4];
-            stream.Read(bytes, 0, 4);
+            Read(bytes, 0, 4);
             return BitConverter.ToSingle(bytes);
         }
 
-        public static Vector ReadVector(FileStream stream)
+        public string ReadString()
+        {
+            byte[] bytes = new byte[4];
+            Read(bytes, 0, 4);
+            int length = BitConverter.ToInt32(bytes);
+
+            byte[] string_data = new byte[length];
+            Read(string_data, 0, length);
+            return Encoding.UTF8.GetString(string_data);
+        }
+
+        public vector ReadVector()
         {
             // Cuts the top off
             byte[] bytes = new byte[4];
-            stream.Read(bytes, 0, 4);
+            Read(bytes, 0, 4);
             int length = BitConverter.ToInt32(bytes);
-
-            Vector result = new();
+            vector result = new();
             for (int i = 0; i < length; i++) {
-                stream.Read(bytes, 0, 4);
+                Read(bytes, 0, 4);
                 result[i] = BitConverter.ToSingle(bytes);
             }
 
             return result;
         }
     }
+
 }
