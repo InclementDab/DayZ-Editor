cc1c532d9e33df8ab2cbb8e04d427cbdfbd40f14
Tyler Paul <tylerpaul2329@gmail.com>
2021-08-11 22:25:43 -0400

    Added ReadInt and ReadFloat to data types

--- BinEditor/Enfusion-Deserializer/Enfusion-Deserializer/Program.cs
+++ BinEditor/Enfusion-Deserializer/Enfusion-Deserializer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Enfusion_Deserializer
 {
@@ -6,7 +7,36 @@ namespace Enfusion_Deserializer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            EditorSaveData data = new();
+
+            FileStream stream = new FileStream("P:\\profiles\\Client\\test.bin", FileMode.Open, FileAccess.Read);
+            Console.WriteLine(stream.Length);
+
+            
+            Console.WriteLine(EditorSaveData.ReadInt(stream));
+            Console.WriteLine(EditorSaveData.ReadInt(stream));
+            Console.WriteLine(EditorSaveData.ReadFloat(stream));
+
+            Console.ReadKey();
+        }
+    }
+
+    public class EditorSaveData
+    {
+        public int test;
+
+        public static int ReadInt(FileStream stream)
+        {
+            byte[] bytes = new byte[4];
+            stream.Read(bytes, 0, 4);
+            return BitConverter.ToInt32(bytes);
+        }
+
+        public static float ReadFloat(FileStream stream)
+        {
+            byte[] bytes = new byte[4];
+            stream.Read(bytes, 0, 4);
+            return BitConverter.ToSingle(bytes);
         }
     }
 }
