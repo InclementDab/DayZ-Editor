1befe761efbca70d7ed2206e6c50114e70d1fda1
Gormirn <78990875+Gormirn@users.noreply.github.com>
2021-12-02 23:06:07 -0500

    Added water check for camera teleporting to keep from going underwater.

--- DayZEditor/Scripts/4_World/DayZEditor/classes/Editor.c
+++ DayZEditor/Scripts/4_World/DayZEditor/classes/Editor.c
@@ -478,13 +478,22 @@ class Editor
 						}
 					}
 				} else {
-					vector pos = Vector(CurrentMousePosition[0], GetGame().SurfaceY(CurrentMousePosition[0], CurrentMousePosition[2]), CurrentMousePosition[2]);
-					vector current_pos = m_EditorCamera.GetPosition();
-					float camera_distance_to_ground = GetGame().SurfaceY(current_pos[0], current_pos[2]);
-					float camera_y_offset = current_pos[1] - camera_distance_to_ground;										
-					vector new_camera_pos = Vector(pos[0],  pos[1] + camera_y_offset, pos[2]);
+					vector mouse_pos = Vector(CurrentMousePosition[0], GetGame().SurfaceY(CurrentMousePosition[0], CurrentMousePosition[2]), CurrentMousePosition[2]);
+					vector camera_current_pos = m_EditorCamera.GetPosition();
+					float camera_surface_y = GetGame().SurfaceY(camera_current_pos[0], camera_current_pos[2]);
+					float camera_y_offset = camera_current_pos[1] - camera_surface_y;										
+					vector new_camera_pos = Vector(mouse_pos[0],  mouse_pos[1] + camera_y_offset, mouse_pos[2]);
+					vector water_camera_pos = Vector(mouse_pos[0],  camera_current_pos[1], mouse_pos[2]);
 					
-					m_EditorCamera.SendToPosition(new_camera_pos);
+					// check if water is under mouse, to stop from teleporting under water			
+					if (IsSurfaceWater(mouse_pos)) {
+						m_EditorCamera.SendToPosition(water_camera_pos);
+						//Print("Water");
+						
+					} else{
+						m_EditorCamera.SendToPosition(new_camera_pos);
+						//Print("Land");
+					}
 				}
 				
 				break;
@@ -509,7 +518,13 @@ class Editor
 		
 		return false;
 	}
-
+	
+	bool IsSurfaceWater( vector position )
+	{
+		CGame game = GetGame();
+		return game.SurfaceIsSea( position[0], position[2] ) || game.SurfaceIsPond( position[0], position[2] );
+	}
+	
 	bool OnMouseRelease(int button)
 	{
 		return false;
