daea4a727a47a110ae3995cd85689102fb368862
Tyler Paul <tylerpaul2329@gmail.com>
2021-12-19 13:49:56 -0500

    Adjusted camera position

--- DayZEditor/Scripts/5_Mission/DayZEditor/dayzintroscene.c
+++ DayZEditor/Scripts/5_Mission/DayZEditor/dayzintroscene.c
@@ -30,8 +30,8 @@ modded class DayZIntroScene
 	{
 		delete m_Character;
 		
-		m_CharacterPos = m_Camera.GetPosition() + m_Camera.GetDirection() * 5;
-		m_CharacterPos = m_CharacterPos + m_Camera.GetDirection() * vector.Forward * 0.2;
+		m_CharacterPos = m_Camera.GetPosition() + m_Camera.GetDirection() * 5.5;
+		m_CharacterPos = m_CharacterPos + m_Camera.GetDirection() * vector.Forward * -3.0;
 		m_CharacterPos = m_CharacterPos + m_Camera.GetDirection() * vector.Up * -0.3;
 	
 		m_FunnyMeme = GetGame().CreateObject("DSLRCamera", m_CharacterPos, true);
