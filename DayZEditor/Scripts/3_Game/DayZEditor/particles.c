67c68de441448e2fcedfde4f9ca67b6effab5fe2
Gormirn <78990875+Gormirn@users.noreply.github.com>
2021-12-20 20:25:56 -0500

    Added fog particle for mainmenu scene

--- DayZEditor/Scripts/3_Game/DayZEditor/particles.c
+++ DayZEditor/Scripts/3_Game/DayZEditor/particles.c
@@ -1,6 +1,7 @@
 modded class ParticleList 
 {
 	static const int SNOW = RegisterParticle("DayZEditor/Editor/particles/", "new_snow");
+	static const int SNOWFOG = RegisterParticle("DayZEditor/Editor/particles/", "fog_maybe");
 }
 
 class Snow : EffectParticle 
@@ -8,5 +9,13 @@ class Snow : EffectParticle
 	void Snow()
 	{
 		SetParticleID(ParticleList.SNOW);
+	}
+}
+
+class SnowFog : EffectParticle
+{
+	void SnowFog()
+	{
+		SetParticleID(ParticleList.SNOWFOG);
 	}
 }
