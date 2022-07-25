365fd2f905dd30a06c068592c1de865f474c43cb
Tyler Paul <tylerpaul2329@gmail.com>
2022-07-24 23:11:11 -0400

    P now copies selected objects to clipboard too

--- DayZEditor/Scripts/4_World/DayZEditor/classes/Commands/Clipboard/EditorCopyPositionCommand.c
+++ DayZEditor/Scripts/4_World/DayZEditor/classes/Commands/Clipboard/EditorCopyPositionCommand.c
@@ -3,8 +3,17 @@ class EditorCopyPositionCommand: EditorCommand
 	protected override bool Execute(Class sender, CommandArgs args)
 	{
 		super.Execute(sender, args);
-		GetEditor().GetEditorHud().CreateNotification(string.Format("Copied current position clipboard!", GetEditor().GetSelectedObjects().Count()), COLOR_GREEN);
-		GetGame().CopyToClipboard(string.Format("%1, %2", GetGame().GetCurrentCameraPosition().ToString(true), GetGame().GetCurrentCameraDirection().ToString(true)));
+		string copy_string = string.Format("[Camera Position]: %1, %2", GetGame().GetCurrentCameraPosition().ToString(true), GetGame().GetCurrentCameraDirection().ToString(true));
+		
+		EditorObjectMap selected_objects = GetEditor().GetSelectedObjects();
+		if (selected_objects.Count() > 0) {
+			foreach (int id, EditorObject editor_object: selected_objects) {
+				copy_string += string.Format("[%1]: pos=%2, ori=%3, scl=%4", editor_object.Name, editor_object.Position.ToString(true), editor_object.Orientation.ToString(true), editor_object.Scale);
+			}
+		}
+		
+		GetEditor().GetEditorHud().CreateNotification(string.Format("Copied position data to clipboard!", GetEditor().GetSelectedObjects().Count()), COLOR_GREEN);
+		GetGame().CopyToClipboard(copy_string);
 		return true;
 	}
 
