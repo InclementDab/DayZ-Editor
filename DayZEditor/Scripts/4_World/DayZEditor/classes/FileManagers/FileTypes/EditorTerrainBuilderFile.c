14e4e77ae2bde5c7012107ae9d058d818db90856
Tyler Paul <tylerpaul2329@gmail.com>
2022-08-14 15:47:03 -0400

    fixes for tb placement

--- DayZEditor/Scripts/4_World/DayZEditor/classes/FileManagers/FileTypes/EditorTerrainBuilderFile.c
+++ DayZEditor/Scripts/4_World/DayZEditor/classes/FileManagers/FileTypes/EditorTerrainBuilderFile.c
@@ -11,12 +11,19 @@ class EditorTerrainBuilderFile: EditorFileType
 			return;
 		}
 		
-		vector terrainbuilder_offset = Vector(200000, 0, 0);
 		foreach (EditorObjectData editor_object: data.EditorObjects) {
 			// "construction_house2";206638.935547;6076.024414;146.000015;0.000000;0.000000;1.000000;
 			// Name, X, Y, Yaw, Pitch, Roll, Scale, Relative Height			
 			vector position = editor_object.BottomCenter;		
 			//if (height_type == HeightType.RELATIVE)
+			
+			// handle offset without float rounding
+			string x_position = position[0].ToString();
+			array<string> vals = {};
+			x_position.Split(".", vals);
+			int x_int = vals[0].ToInt();
+			x_int += 200000;			
+			
 			position[1] = position[1] - GetGame().SurfaceY(position[0], position[2]);
 			
 			string type = editor_object.Type;
@@ -35,8 +42,7 @@ class EditorTerrainBuilderFile: EditorFileType
 			EditorLog.Debug("Exporting %1 with model of %2", editor_object.Type, model_name);
 			
 			vector orientation = editor_object.Orientation;
-			position += terrainbuilder_offset;
-			string line = string.Format("\"%1\";%2;%3;%4;%5;%6;%7;%8;", model_name, position[0], position[2], orientation[0], orientation[2], orientation[1], editor_object.Scale, position[1]);
+			string line = string.Format("\"%1\";%2;%3;%4;%5;%6;%7;%8;", model_name, x_int.ToString() + "." + vals[1], position[2], orientation[0], orientation[2], orientation[1], editor_object.Scale, position[1]);
 			FPrintln(handle, line);
 		}
 		
