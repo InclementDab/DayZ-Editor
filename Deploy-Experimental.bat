b4307eedcc2c85291ccbb73e29c92585739bbebf
InclementDab <tylerpaul2329@gmail.com>
2023-07-14 16:52:35 -0400

    Update Deploy-Experimental.bat

--- Deploy-Experimental.bat
+++ Deploy-Experimental.bat
@@ -2,7 +2,7 @@
 
 echo Deploying to Experimental. Are you sure?
 
-start /W "Build PBO" "%CD%/DayZEditor/Workbench/Batchfiles/ZBinarizeDeploy.bat"
+REM start /W "Build PBO" "%CD%/DayZEditor/Workbench/Batchfiles/ZBinarizeDeploy.bat"
 echo Uploading to Workshop
 cd "%CD%/DayZEditor/Workbench/Deployment"
 start /W "Upload to Workshop" Deployment.exe experimental
