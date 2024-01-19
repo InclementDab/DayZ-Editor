e85f040b3f56ae1bf1b1738f82f91c1d9ec44a55
Tyler Paul <tylerpaul2329@gmail.com>
2024-01-19 15:16:53 -0500

    No one can touch how cool this is

--- Tools/DirectDrawConverter/Program.cs
+++ Tools/DirectDrawConverter/Program.cs
@@ -6,61 +6,34 @@ using DirectDrawConverter;
 using PaintDotNet;
 using Svg;
 
-const int ROW_COUNT = 10;
+
 const int IMAGE_MAX_SIZE = 512; // this is the space allocated to each image
+const int IMAGE_SIZE = 256;
 
 string folder_output = "P:\\Editor\\GUI\\icons";
 DirectoryInfo dest_directory = Directory.CreateDirectory(folder_output);
 DirectoryInfo source_directory = Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\svgs");
 
-foreach (DirectoryInfo directory in source_directory.EnumerateDirectories())
-{
+foreach (DirectoryInfo directory in source_directory.EnumerateDirectories()) {
     string output_file = dest_directory + $"\\{directory.Name}.dds";
     string enfusion_output_file = output_file.Replace("P:\\", "");
     enfusion_output_file = enfusion_output_file.Replace("dds", "edds");
-    Dictionary<string, Bitmap> source_bitmaps = new Dictionary<string, Bitmap>();
 
-    foreach (FileInfo file in directory.GetFiles())
-    {
-        Console.WriteLine($"Loading file {file}");
-        // workbench doesnt like dashes in the name
-        string format_name = file.Name;
-        format_name = format_name.Replace("-", "_");
+    string formatted_directory_name = directory.Name;
+    formatted_directory_name = formatted_directory_name.Replace("-", "_");
 
-        string file_name = Path.GetFileNameWithoutExtension(format_name);
-        switch (file.Extension)
-        {
-            case ".png": {
-                source_bitmaps[file_name] = new Bitmap(file.ToString());
-                break;
-            }
+    File.Delete(dest_directory + $"\\{formatted_directory_name}.imageset");
 
-            case ".svg": {
-                SvgDocument doc = SvgDocument.Open(file.ToString());
+    IEnumerable<FileInfo> files = directory.GetFiles("*.svg");
+    int row_count = (int)Math.Floor(Math.Sqrt(files.Count()));
 
-                double w_aspect = 1.0;
-                double h_aspect = 1.0;
-                if (doc.Bounds.Width > IMAGE_MAX_SIZE || doc.Bounds.Height > IMAGE_MAX_SIZE) {
-                    w_aspect = doc.Bounds.Width / IMAGE_MAX_SIZE;
-                }
-                // we bound to the larger of the two aspects
-                if (w_aspect > 1.0) {
-                    h_aspect = 1.0 / w_aspect;
-                    w_aspect = 1.0;
-                }
+    int imageset_width = Math.Min(row_count, files.Count()) * IMAGE_SIZE;
+    int imageset_height = (int)(Math.Ceiling(files.Count() / (decimal)row_count) * IMAGE_SIZE);
 
-                source_bitmaps[file_name] = doc.Draw((int)(IMAGE_MAX_SIZE * w_aspect), (int)(IMAGE_MAX_SIZE * h_aspect));
-                break;
-            }
-        }
-    }
-
-    int width = Math.Min(ROW_COUNT, source_bitmaps.Count) * IMAGE_MAX_SIZE;
-    int height = (int)(Math.Ceiling((decimal)source_bitmaps.Count / (decimal)ROW_COUNT) * (decimal)IMAGE_MAX_SIZE);
-    Bitmap output_map = new(width, height);
+    Surface surface = new(imageset_width, imageset_height);
     ImageSet image_set = new() {
-        Name = directory.Name,
-        RefSize = new Vec2D(width, height),
+        Name = formatted_directory_name,
+        RefSize = new Vec2D(imageset_width, imageset_height),
         Textures = {
             new() {
                 mpix = 1,
@@ -75,52 +48,77 @@ foreach (DirectoryInfo directory in source_directory.EnumerateDirectories())
         ChangeDate = (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds
     };
 
+    DateTime file_start = DateTime.Now;
+    Console.WriteLine($"Loading {files.Count()} files");
+
     int x_offset = 0, y_offset = 0;
-    foreach (KeyValuePair<string, Bitmap> source_bitmap in source_bitmaps) {
-        int w_centering_offset = (IMAGE_MAX_SIZE - source_bitmap.Value.Size.Width) / 2;
-        int h_centering_offset = (IMAGE_MAX_SIZE - source_bitmap.Value.Size.Height) / 2;
-        for (int x = 0; x < source_bitmap.Value.Size.Width; x++) {
-            for (int y = 0; y < source_bitmap.Value.Size.Height; y++) {
-                Color color = source_bitmap.Value.GetPixel(x, y);
-                output_map.SetPixel(x + x_offset + w_centering_offset, y + y_offset + h_centering_offset, Color.FromArgb(color.A, 255, 255, 255));
+    foreach (FileInfo file in files) {
+        // workbench doesnt like dashes in the name
+        string format_name = file.Name;
+        format_name = format_name.Replace("-", "_");
+        string file_name = Path.GetFileNameWithoutExtension(format_name);
+
+        SvgDocument doc = SvgDocument.Open(file.ToString());
+        double w_aspect = 1.0;
+        double h_aspect = 1.0;
+        if (doc.Bounds.Width > IMAGE_MAX_SIZE || doc.Bounds.Height > IMAGE_MAX_SIZE) {
+            w_aspect = doc.Bounds.Width / IMAGE_MAX_SIZE;
+        }
+        // we bound to the larger of the two aspects
+        if (w_aspect > 1.0) {
+            h_aspect = 1.0 / w_aspect;
+            w_aspect = 1.0;
+        }
+
+        int width = (int)(IMAGE_SIZE * w_aspect);
+        int height = (int)(IMAGE_SIZE * h_aspect);
+        Bitmap bitmap = doc.Draw(width, height);
+
+        int w_centering_offset = (IMAGE_SIZE - width) / 2;
+        int h_centering_offset = (IMAGE_SIZE - height) / 2;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                ColorBgra color = ColorBgra.White;
+                color.A = bitmap.GetPixel(x, y).A;
+                surface[new Point(x + x_offset + w_centering_offset, y + y_offset + h_centering_offset)] = color;
             }
         }
 
-        ImageSet.ImageSetDef image_set_def = new() {
-            Name = source_bitmap.Key,
+        image_set.Images.Add(new ImageSet.ImageSetDef() {           
+            Name = file_name,
             Pos = new Vec2D(x_offset + w_centering_offset, y_offset + h_centering_offset),
-            Size = new Vec2D(source_bitmap.Value.Size.Width, source_bitmap.Value.Size.Height),
+            Size = new Vec2D(width, height),
             Flags = 0
-        };
+        });
 
-        image_set.Images.Add(image_set_def);
-
-        x_offset += IMAGE_MAX_SIZE;
-        if (x_offset >= ROW_COUNT * IMAGE_MAX_SIZE) {
+        x_offset += IMAGE_SIZE;
+        if (x_offset >= row_count * IMAGE_SIZE) {
             x_offset = 0;
-            y_offset += IMAGE_MAX_SIZE;
+            y_offset += IMAGE_SIZE;
         }
     }
 
-    File.Delete(dest_directory + $"\\{directory.Name}.imageset");
     File.WriteAllText(dest_directory + $"\\{directory.Name}.imageset", image_set.ToStringy());
-    //File.WriteAllText(dest_directory + $"\\{OUTPUT_NAME}.edds.meta", meta_file.ToStringy());
-    Console.WriteLine($"Outputting to {output_file}");
 
+    Console.WriteLine($"ImageSet generated {dest_directory + $"\\{directory.Name}.imageset"} complete, took {(DateTime.Now - file_start).TotalSeconds}");
     File.Delete(output_file);
+
+    DateTime dds_start = DateTime.Now;
     Stream dds_stream = new FileStream(output_file, FileMode.CreateNew);
     DdsFile.Save(
         dds_stream,
-        DdsFileFormat.BC7,
-        DdsErrorMetric.Uniform,
+        DdsFileFormat.BC3,
+        DdsErrorMetric.Perceptual,
         BC7CompressionSpeed.Medium,
-        true,
-        true,
+        false,
+        false,
         ResamplingAlgorithm.Bilinear,
-        Surface.CopyFromBitmap(output_map),
+        surface,
         ProgressChanged);
 
     dds_stream.Close();
+
+    Console.WriteLine($"Output to {output_file} complete, took {(DateTime.Now - dds_start).TotalSeconds}");
 }
 
 void ProgressChanged(object sender, ProgressEventArgs e)
