fd086055fa19d6515fa958bfbd5103cc7ba3052c
Tyler Paul <tylerpaul2329@gmail.com>
2024-01-22 13:42:08 -0500

    Update EditorStaticPlaceableItem.c

--- Editor/Scripts/3_Game/Editor/Classes/SerializableBase/EditorPlaceableObjectData/EditorStaticPlaceableItem.c
+++ Editor/Scripts/3_Game/Editor/Classes/SerializableBase/EditorPlaceableObjectData/EditorStaticPlaceableItem.c
@@ -5,11 +5,13 @@ class EditorStaticPlaceableItem: EditorPlaceable
 	void EditorStaticPlaceableItem(string uuid, string display_name, string icon)
 	{
 		m_Model = uuid;
+		m_Model.Replace("\\", "/");
 	}
 	
 	override Object CreateObject(vector transform[4], int flags = ECE_LOCAL)
 	{
-		Object object = GetGame().CreateStaticObjectUsingP3D(m_Model, transform[3], vector.Zero, 1.0);
+		Print(m_Model);
+		Object object = GetGame().CreateStaticObjectUsingP3D(m_Model, transform[3], transform[2].VectorToAngles(), 1.0);
 		object.SetTransform(transform);
 		return object;
 	}
