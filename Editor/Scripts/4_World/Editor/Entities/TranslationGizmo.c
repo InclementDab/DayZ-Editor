3bf21111a53b895431c4b0451a528514a27545af
Tyler Paul <tylerpaul2329@gmail.com>
2024-01-23 17:02:59 -0500

    working on gizmo

--- Editor/Scripts/4_World/Editor/Entities/TranslationGizmo.c
+++ Editor/Scripts/4_World/Editor/Entities/TranslationGizmo.c
@@ -1,7 +1,19 @@
+class RayView: ScriptView
+{
+	override string GetLayoutFile()
+	{
+		return "Editor\\GUI\\layouts\\ray.layout";
+	}
+}
+
 class TranslationGizmo: House
 {
+	protected ref array<ref RayView> m_RayViews = {};
+	
 	void TranslationGizmo()
 	{
+		m_RayViews.Insert(new RayView());
+		
 		GetGame().GetUpdateQueue(CALL_CATEGORY_GUI).Insert(OnUpdate);
 	}
 	
@@ -16,14 +28,29 @@ class TranslationGizmo: House
 			return;
 		}
 		
-		vector ray_start = GetGame().GetCurrentCameraPosition();
-		vector ray_end = ray_start + GetGame().GetCurrentCameraDirection() * 3000;
-		Object hit_object;
-		vector position, normal;
-		float fraction;
+		vector transform[4];
+		GetRenderTransform(transform);
+		
+		vector start_point = GetGame().GetScreenPos(transform[3]);
+		vector end_point = GetGame().GetScreenPos(GetMemoryPointPos("x").Multiply4(transform));
+		
+		RayView x = m_RayViews[0];
+		
+		x.GetLayoutRoot().SetSize(Math.AbsFloat(end_point[0] - start_point[0]), 10);
+		
+		float theta = Math.Atan2(end_point[1], start_point[1]);
+		
+		x.GetLayoutRoot().SetPos(start_point[0], start_point[1]);
+		
+		vector camera_transform[3];
+		GetDayZGame().GetEditor().GetCamera().GetTransform(camera_transform);
+		
+		vector inv_transform[3];
+		Math3D.MatrixMultiply3(camera_transform, transform, inv_transform);
+		
+		vector angles = Math3D.MatrixToAngles(inv_transform);
 		
 		
-		//DayZPhysics.RayCastBullet(ray_start, ray_end, PhxInteractionLayers.DEFAULT, null, hit_object, position, normal, fraction);
-		//Print(hit_object);		
+		x.GetLayoutRoot().SetRotation(angles[0], angles[1], angles[2]);
 	}
 }
