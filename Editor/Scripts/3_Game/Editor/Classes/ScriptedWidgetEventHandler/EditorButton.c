429f489892a570220bdb627ed68c6e11abfbda5b
Tyler Paul <tylerpaul2329@gmail.com>
2024-01-29 13:50:05 -0500

    Refactoring editorbutton

--- Editor/Scripts/3_Game/Editor/Classes/ScriptedWidgetEventHandler/EditorButton.c
+++ Editor/Scripts/3_Game/Editor/Classes/ScriptedWidgetEventHandler/EditorButton.c
@@ -1,48 +1,37 @@
 class EditorButton: ScriptedWidgetEventHandler
-{	
-	protected CommandNode m_Node;
-	
+{		
 	reference string Node;
 	
 	protected Symbols m_Icon;
 	protected Widget m_LayoutRoot;
+	protected TextWidget m_TextWidget;
 	protected ButtonWidget m_Button;
 	protected ImageWidget m_IconWidget;
+	protected TreeNode m_Node;
 	
 	void OnWidgetScriptInit(Widget w)
 	{
 		m_LayoutRoot = w;
 		m_LayoutRoot.SetHandler(this);
 	
-		m_Button = FindWidget<ButtonWidget>.SearchDown(m_LayoutRoot, "Button");
-		if (!m_Button) {
-			Error("Button widget not found below " + m_LayoutRoot.GetName());
-			return;
-		}
-		
+		m_Button = FindWidget<ButtonWidget>.SearchDown(m_LayoutRoot, "Button");		
 		m_Button.SetHandler(this);
 		
-		m_IconWidget = FindWidget<ImageWidget>.SearchDown(m_LayoutRoot, "Icon");
-		if (!m_IconWidget) {
-			Error("Icon widget not found below " + m_LayoutRoot.GetName());
-			return;
-		}
+		m_IconWidget = FindWidget<ImageWidget>.SearchDown(m_LayoutRoot, "Icon");		
+		m_TextWidget = FindWidget<TextWidget>.SearchDown(m_LayoutRoot, "Text");
 		
 #ifdef WORKBENCH
 		if (!GetDayZGame()) {
 			return;
 		}
 #endif
-	
-		// This is executing too early because GetEditor hasnt actually been inserted yet!, what doo?>????	
-		if (Node != string.Empty) {	
-			m_Node = GetDayZGame().GetEditor().GetCommand(Node);
-			if (m_Node) {
-				m_Icon = m_Node.GetIcon();
-				m_Node.AfterSelectionChanged.Insert(OnExecuted);
-				GetDayZGame().GetEditor().Select(m_Node);
-				OnExecuted(m_Node, m_Node.GetDefaultState());
-			}
+		
+		m_Node = GetDayZGame().GetEditor().GetNode(Node);
+		if (m_Node) {
+			m_Icon = m_Node.GetIcon();
+			m_Node.AfterSelectionChanged.Insert(OnExecuted);
+			GetDayZGame().GetEditor().Select(m_Node);
+			OnExecuted(m_Node, m_Node.GetDefaultState());
 		}
 	}
 	
@@ -56,96 +45,21 @@ class EditorButton: ScriptedWidgetEventHandler
 		m_IconWidget.LoadImageFile(0, Ternary<Symbol>.If(state, m_Icon.Solid(), m_Icon.Regular()));
 		m_IconWidget.SetImage(0);
 	}
-		
-	Widget GetLayoutRoot()
-	{
-		return m_LayoutRoot;
-	}
-		
-	override bool OnMouseButtonDown(Widget w, int x, int y, int button)
-	{
-		if (button != 0 || !m_Node) {
-			return false;
-		}
-		
-		switch (m_Node.GetShortcutType()) {
-			case ShortcutKeyType.PRESS:
-			case ShortcutKeyType.HOLD: {
-				GetDayZGame().GetEditor().Select(m_Node);
-				return true;
-			}
-		}
-				
-		return false;
-	}
 	
-	override bool OnMouseButtonUp(Widget w, int x, int y, int button)
-	{
-		if (button != 0 || !m_Node) {
-			return false;
-		}
-		
-		switch (m_Node.GetShortcutType()) {
-			case ShortcutKeyType.HOLD: {
-				GetDayZGame().GetEditor().Deselect(m_Node);
-				return true;
-			}
-		}
-				
-		return false;
-	}
-	
-	override bool OnClick(Widget w, int x, int y, int button)
-	{
-		if (button != 0 || !m_Node) {
-			return false;
-		}
-		
-		switch (m_Node.GetShortcutType()) {
-			case ShortcutKeyType.TOGGLE: {
-				GetDayZGame().GetEditor().ToggleSelect(m_Node);
-				return true;
-			}
-		}
-				
-		return false;
-	}
-		
-	override bool OnDoubleClick(Widget w, int x, int y, int button)
-	{
-		if (button != 0 || !m_Node) {
-			return false;
-		}
-		
-		switch (m_Node.GetShortcutType()) {
-			case ShortcutKeyType.DOUBLE: {
-				GetDayZGame().GetEditor().ToggleSelect(m_Node);
-				return true;
-			}
-		}
-		
-		return false;
-	}
-		
 	override bool OnMouseEnter(Widget w, int x, int y)
 	{
-		WidgetAnimator.Animate(m_IconWidget, WidgetAnimatorProperty.COLOR_A, 1.0, 50);
-		
-		if (m_Node) {
-			GetDayZGame().GetEditor().GetHud().SetCursor(m_Icon, m_Node.GetDisplayName(), m_Node.GetShortcutString());
-		}
-		
+		WidgetAnimator.Animate(m_IconWidget, WidgetAnimatorProperty.COLOR_A, 1.0, 50);		
 		return true;
 	}
 	
 	override bool OnMouseLeave(Widget w, Widget enterW, int x, int y)
 	{				
-		GetDayZGame().GetEditor().GetHud().ClearCursor();
-		
-		if (!m_Node.GetEditor().IsSelected(m_Node)) {
-			WidgetAnimator.Animate(m_IconWidget, WidgetAnimatorProperty.COLOR_A, 100.0 / 255.0, 50);
-		}
-		
+		GetDayZGame().GetEditor().GetHud().ClearCursor();		
 		return true;
+	}
+		
+	Widget GetLayoutRoot()
+	{
+		return m_LayoutRoot;
 	}
 }
