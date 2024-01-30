b681c2d548531616acae5c1182ec6ad3df759ce4
Tyler Paul <tylerpaul2329@gmail.com>
2024-01-30 15:55:17 -0500

    MUCH BETTER

--- Editor/Scripts/3_Game/Editor/Classes/ScriptedWidgetEventHandler/NodeWidgetEventHandler/EditorButton.c
+++ Editor/Scripts/3_Game/Editor/Classes/ScriptedWidgetEventHandler/NodeWidgetEventHandler/EditorButton.c
@@ -7,18 +7,20 @@ class EditorButton: NodeWidgetEventHandler
 		}
 		
 		if (state.IsHover()) {
-			//WidgetAnimator.Animate(Icon, WidgetAnimatorProperty.COLOR_A, 1.0, 50);
-			Icon.SetAlpha(1.0);
 			node.GetEditor().GetHud().SetCursor(node.GetIcon(), node.GetDisplayName(), node.GetShortcutString());
-		}
-		
-		if (state.IsActive()) {
-			Icon.LoadImageFile(0, node.GetIcon().Solid());
 		} else {
 			node.GetEditor().GetHud().ClearCursor();
+		}
+		
+		int color = ARGB(100 + state.IsHover() * 155.0, 255, 255, 255);
+		if (state.IsActive()) {
+			color = m_LayoutRoot.GetColor();
+			Icon.LoadImageFile(0, node.GetIcon().Solid());
+		} else {
 			Icon.LoadImageFile(0, node.GetIcon().Regular());
 		}
 		
+		Icon.SetColor(color);		
 		Icon.SetImage(0);
 	}
 }
