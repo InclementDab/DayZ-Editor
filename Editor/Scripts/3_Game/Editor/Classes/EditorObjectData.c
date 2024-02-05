747df0151694f96dfe290a862f2285f8f758f3f4
Tyler Paul <tylerpaul2329@gmail.com>
2024-02-05 01:14:04 -0500

    Create EditorObjectData.c

---
+++ Editor/Scripts/3_Game/Editor/Classes/EditorObjectData.c
@@ -0,0 +1,17 @@
+modded class EditorObjectData
+{
+	ObjectNode CovertToNode()
+	{
+		vector matrix[4];
+		Math3D.MatrixOrthogonalize4(matrix);
+		Object object = EditorNode.CreateObject(Type, matrix);
+		if (!object) {
+			return null;
+		}
+		
+		object.SetPosition(Position);
+		object.SetOrientation(Orientation);
+		object.SetScale(Scale);
+		return new ObjectNode(UUID.Generate(), DisplayName, Symbols.QUESTION, object);
+	}
+}
