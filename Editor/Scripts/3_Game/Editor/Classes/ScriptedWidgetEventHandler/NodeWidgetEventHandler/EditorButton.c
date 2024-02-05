0e6049cb4565e95ca671ad266e07c343e17b575f
Tyler Paul <tylerpaul2329@gmail.com>
2024-02-05 01:22:14 -0500

    Update EditorButton.c

--- Editor/Scripts/3_Game/Editor/Classes/ScriptedWidgetEventHandler/NodeWidgetEventHandler/EditorButton.c
+++ Editor/Scripts/3_Game/Editor/Classes/ScriptedWidgetEventHandler/NodeWidgetEventHandler/EditorButton.c
@@ -2,10 +2,6 @@ class EditorButton: NodeWidgetEventHandler
 {
 	override void OnStateChanged(TreeNode node, TreeNodeState state)
 	{		
-		if (!node.GetEditor().GetHud() || !Icon) {
-			return;
-		}
-		
 		if (state.IsHover()) {
 			if (node.GetState().IsHover()) {
 				node.GetEditor().GetHud().SetCursor(node.GetIcon(), node.GetDisplayName(), node.GetShortcutString());
@@ -14,15 +10,17 @@ class EditorButton: NodeWidgetEventHandler
 			}
 		}
 		
-		int color = ARGB(100 + node.GetState().IsHover() * 155.0, 255, 255, 255);
-		if (node.GetState().IsActive()) {
-			color = m_LayoutRoot.GetColor();
-			Icon.LoadImageFile(0, node.GetIcon().Solid());
-		} else {
-			Icon.LoadImageFile(0, node.GetIcon().Regular());
+		if (Icon) {
+			int color = ARGB(100 + node.GetState().IsHover() * 155.0, 255, 255, 255);
+			if (node.GetState().IsActive()) {
+				color = m_LayoutRoot.GetColor();
+				Icon.LoadImageFile(0, node.GetIcon().Solid());
+			} else {
+				Icon.LoadImageFile(0, node.GetIcon().Regular());
+			}
+			
+			Icon.SetColor(color);		
+			Icon.SetImage(0);
 		}
-		
-		Icon.SetColor(color);		
-		Icon.SetImage(0);
 	}
 }
