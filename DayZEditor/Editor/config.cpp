426b057c72d71dac9f901631ff1b88344b5096e7
TheDevilsKnock <uk.dicarlo@gmail.com>
2024-02-24 03:31:47 +0100

    Trees rotation + pls remove this EGG

--- DayZEditor/Editor/config.cpp
+++ DayZEditor/Editor/config.cpp
@@ -15,77 +15,77 @@ class CfgVehicles
 	class Static;
 	class HouseNoDestruct;
 
-	class DSLRCamera: HouseNoDestruct
+	class DSLRCamera : HouseNoDestruct
 	{
 		scope = 1;
 		model = "\DayZEditor\Editor\CameraNormal.p3d";
 	};
 
-	class DSLRCameraChristmas: HouseNoDestruct
+	class DSLRCameraChristmas : HouseNoDestruct
 	{
 		scope = 1;
 		model = "\DayZEditor\Editor\Camera_xmas_Normal.p3d";
 	};
 
-	class DSLRCameraAnniversary: HouseNoDestruct
+	class DSLRCameraAnniversary : HouseNoDestruct
 	{
 		scope = 1;
 		model = "\DayZEditor\Editor\Camera.p3d";
 	};
 
-	class XmasGiftRed1: HouseNoDestruct
+	class XmasGiftRed1 : HouseNoDestruct
 	{
 		scope = 1;
 		model = "\DayZEditor\Editor\xmas_gift_red1.p3d";
 	};
-	class XmasGiftRed2: HouseNoDestruct
+	class XmasGiftRed2 : HouseNoDestruct
 	{
 		scope = 1;
 		model = "\DayZEditor\Editor\xmas_gift_red2.p3d";
 	};
-	class XmasGiftGreen1: HouseNoDestruct
+	class XmasGiftGreen1 : HouseNoDestruct
 	{
 		scope = 1;
 		model = "\DayZEditor\Editor\xmas_gift_green1.p3d";
 	};
-	class XmasGiftGreen2: HouseNoDestruct
+	class XmasGiftGreen2 : HouseNoDestruct
 	{
 		scope = 1;
 		model = "\DayZEditor\Editor\xmas_gift_green2.p3d";
 	};
-	class XmasGiftBlue1: HouseNoDestruct
+	class XmasGiftBlue1 : HouseNoDestruct
 	{
 		scope = 1;
 		model = "\DayZEditor\Editor\xmas_gift_blue1.p3d";
 	};
-	class XmasGiftBlue2: HouseNoDestruct
+	class XmasGiftBlue2 : HouseNoDestruct
 	{
 		scope = 1;
 		model = "\DayZEditor\Editor\xmas_gift_blue2.p3d";
 	};
 
 	class Inventory_Base;
-	class GizmoBase: Inventory_Base
+	class GizmoBase : Inventory_Base
 	{
 		scope = 0;
 		hiddenSelections[] = {"main"};
 		shadow = 0;
 	};
-	class GizmoArrow: GizmoBase
+	class GizmoArrow : GizmoBase
 	{
 		scope = 0;
 		model = "\DayZEditor\Editor\TranslationWidget.p3d";
 		shadow = 0;
 	};
 
-	class GizmoCenter: GizmoBase
+	class GizmoCenter : GizmoBase
 	{
 		scope = 0;
 		model = "\DayZEditor\Editor\TranslationCenter.p3d";
 		shadow = 0;
 	};
 
-	class RotationWidget: Inventory_Base
+	class RotationWidget : Inventory_Base
 	{
 		scope = 1;
 		itemSize[]={16,16};
@@ -94,7 +94,7 @@ class CfgVehicles
 		shadow = 0;
 	};
 
-	class BrushBase: Inventory_Base
+	class BrushBase : Inventory_Base
 	{
 		scope = 1;
 		model = "\DayZEditor\Editor\BrushBase.p3d";
@@ -103,7 +103,7 @@ class CfgVehicles
 		shadow = 0;
 	};
 
-	class BoundingBoxBase: HouseNoDestruct
+	class BoundingBoxBase : HouseNoDestruct
 	{
 		scope = 1;
 		model = "\DayZEditor\Editor\BoundingBoxBase.p3d";
@@ -111,19 +111,19 @@ class CfgVehicles
 		shadow = 0;
 	};
 
-	class EditorLootPoint: HouseNoDestruct
+	class EditorLootPoint : HouseNoDestruct
 	{
 		scope=2;
 		model="DayZEditor\editor\DebugCylinder.p3d";
 	};
 
-	class EGG: HouseNoDestruct
+	class Egg : HouseNoDestruct
 	{
 		scope=2;
 		model="DZ\gear\food\eeg.p3d";
 	};
 
-	class DZE_Logo_Graffiti: HouseNoDestruct
+	class DZE_Logo_Graffiti : HouseNoDestruct
 	{
 		scope=2;
 		displayName="DZE_Logo_Graffiti";
@@ -131,11 +131,11 @@ class CfgVehicles
 		model="\DayZEditor\Editor\Logo_Graffiti.p3d";
 		shadow = 0;
 	};
-/*
-	class CinematicCamera: HouseNoDestruct
-	{
-		scope = 1;
-		model = "\DayZEditor\Editor\CinematicCamera.p3d";
-		hiddenSelections[] = {"CinematicCameraHighlight"};
-	};*/
+	/*
+		class CinematicCamera: HouseNoDestruct
+		{
+			scope = 1;
+			model = "\DayZEditor\Editor\CinematicCamera.p3d";
+			hiddenSelections[] = {"CinematicCameraHighlight"};
+		};*/
 };
