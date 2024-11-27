8850028edfc3b365428e1364b8b53cc9645cf49c
Tyler Paul <tylerpaul2329@gmail.com>
2024-11-27 11:47:23 -0500

    Working on a console audit command

---
+++ DayZEditor/Scripts/4_World/DayZEditor/classes/Commands/Objects/EditorConsoleAuditCommand.c
@@ -0,0 +1,32 @@
+[RegisterEditorCommand(EditorConsoleAuditCommand)]
+class EditorConsoleAuditCommand: EditorCommand
+{
+	override bool Execute(Class sender, CommandArgs args)
+	{
+		super.Execute(sender, args);
+		
+		EditorObjectMap objects = m_Editor.GetPlacedObjects();
+		foreach (EditorObject object: objects) {
+			
+		}
+		
+		auto deleted_objects = m_Editor.GetDeletedObjects();
+		if (deleted_objects.Count() > 0) {
+			
+		}
+		
+		m_Editor.GetEditorHud().ShowMessageBox();
+		
+		return true;
+	}
+	
+	protected void MessageBoxCallback(DialogResult result)
+	{
+		
+	}
+	
+	override string GetName()
+	{
+		return "Run Console Audit";
+	}
+}
