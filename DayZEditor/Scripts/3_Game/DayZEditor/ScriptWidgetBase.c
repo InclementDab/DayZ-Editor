958a62dcf540f99b83478b6715600f13e7000718
Tyler Paul <tylerpaul2329@gmail.com>
2024-12-05 19:44:38 -0500

    Added compass
    - Added compass to show directions easier

---
+++ DayZEditor/Scripts/3_Game/DayZEditor/ScriptWidgetBase.c
@@ -0,0 +1,20 @@
+class ScriptWidgetBase: ScriptedWidgetEventHandler
+{
+	protected Widget m_LayoutRoot;
+	
+	void OnWidgetScriptInit(Widget w)
+	{
+		m_LayoutRoot = w;
+		if (m_LayoutRoot) {
+			ScriptView.LoadWidgetsAsVariables(this, new PropertyTypeHashMap(Type()), w);
+		}
+		
+		if (g_Game) {
+			g_Game.GetUpdateQueue(CALL_CATEGORY_GUI).Insert(Update);
+		}
+	}
+	
+	protected void Update(float timeslice)
+	{
+	}
+}
