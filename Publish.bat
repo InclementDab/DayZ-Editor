49ed336c360716142ead316d0ae875f846b8b959
Tyler Paul <tylerpaul2329@gmail.com>
2025-12-16 08:06:22 -0500

    eek

--- Publish.bat
+++ Publish.bat
@@ -1,3 +1,3 @@
 @echo off
 
-C:\Dev\proj\DayZ-DabsLabs-Server\External\Build\bin\Release\net9.0\publish\Build.exe --input="P:\DayZEditor" --output="P:\Mods\@DayZEditor" --id=2648049375
+C:\Dev\proj\DayZ-DabsLabs-Server\External\Build\bin\Release\net9.0\publish\Build.exe --input="P:\DayZEditor" --output="P:\Mods\@DayZEditor" --id=2648049375 --name="DayZ Editor"
