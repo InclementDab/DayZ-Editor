291599e38d4fb47214b6fe55e52c3ba4cc491edc
Tyler Paul <tylerpaul2329@gmail.com>
2026-01-02 16:47:18 -0500

    update publish batch files to make more sense

---
+++ PublishExperimental.bat
@@ -0,0 +1,1 @@
+C:\Dev\proj\DayZ-DabsLabs-Server\External\Build\bin\Release\net9.0\publish\Build.exe --input="P:\DayZEditor" --output="P:\Mods\@DayZEditor" --id=2648049375 --name="DayZ-Editor-Experimental"
