2e879d595ad21276aeb6b0f143f1d8b0c7265836
TheDevilsKnock <uk.dicarlo@gmail.com>
2026-05-28 03:09:52 +0200

    refactor: rework changelog dialog layout and pagination controls
    Layout (EditorChangelogDialog.layout):
    - Rebuilt dialog structure: scroll area → nav buttons → donation
      text → action buttons → "Don't Show Again" row
    - Widened NeverShowAgainText from 150 → 250 px to prevent text
      clipping
    
    Script (EditorOneTimeDonationDialog.c):
    - Added PrevButton, NextButton, PageIndicatorText widget bindings
    - Added UpdateNavigationButtons() to sync page indicator and
      enable/disable prev/next based on current index
    - Added OnPrevButton() / OnNextButton() relay command handlers
    - GetLayoutFile() updated to EditorChangelogDialog.layout

--- DayZEditor/Scripts/4_World/DayZEditor/gui/Dialogs/EditorOneTimeDonationDialog.c
+++ DayZEditor/Scripts/4_World/DayZEditor/gui/Dialogs/EditorOneTimeDonationDialog.c
@@ -1,13 +1,3 @@
-class RichTextPrefab: ScriptView
-{
-	TextListboxWidget ListBox;
-	
-	override string GetLayoutFile()
-	{
-		return "DayZEditor\\GUI\\layouts\\prefabs\\RichTextPrefab.layout";
-	}
-}
-
 class Payload_Changelog: Managed
 {
 	int Id;
@@ -20,10 +10,12 @@ class Payload_Changelog: Managed
 class ChangelogRestCallback: RestCallbackBase
 {
 	protected ref ScriptCaller m_Callback;
+	protected ref ScriptCaller m_ErrorCallback;
 	
-	void ChangelogRestCallback(ScriptCaller callback)
+	void ChangelogRestCallback(ScriptCaller callback, ScriptCaller error_callback = null)
 	{
 		m_Callback = callback;
+		m_ErrorCallback = error_callback;
 	}
 
 	override void OnSuccess(string data, int dataSize)
@@ -32,67 +24,199 @@ class ChangelogRestCallback: RestCallbackBase
 		WebApiLog.Log(string.Format("%1 returned success from api, completed in %2ms", Type(), GetGame().GetTime() - m_CallTime));
 
 		JsonSerializer serializer = new JsonSerializer();
-		Payload_Changelog payload = new Payload_Changelog();
+		array<ref Payload_Changelog> payload = {};
 		string error;
 		if (serializer.ReadFromString(payload, data, error) && m_Callback) {
 			m_Callback.Invoke(payload);
+		} else if (m_ErrorCallback) {
+			Error(error);
+			m_ErrorCallback.Invoke();
+		}
+	}
+
+	override void OnTimeout()
+	{
+		super.OnTimeout();
+
+		if (m_ErrorCallback) {
+			m_ErrorCallback.Invoke();
+		}
+	}
+
+	override void OnError(int errorCode)
+	{
+		super.OnError(errorCode);
+
+		if (m_ErrorCallback) {
+			m_ErrorCallback.Invoke();
 		}
 	}
 }
 
 class EditorOneTimeDonationDialog: EditorDialogBase
 {
-	protected ref TextBoxPrefab m_RichTitle, m_RichTitle2;
-	protected ref RichTextPrefab m_RichText;
-	protected ref MessageBoxPrefab m_TextBox, m_TextBox2;
-	protected ref CheckBoxPrefab m_NeverShowAgain;
-	protected bool m_NeverShowAnymore;
-		
+	protected TextListboxWidget ListBox;
+	protected ButtonWidget PrevButton, NextButton;
+	protected TextWidget PageIndicatorText;
+	protected CheckBoxWidget NeverShowAgainCheckBox;
+	protected ref array<ref Payload_Changelog> m_Changelogs = {};
+	protected int m_CurrentChangelogIndex;
+
 	protected string m_Text;
-	
+
 	void EditorOneTimeDonationDialog(string title)
-	{			
+	{
 		EditorWebApi web_api = GetEditor().GetWebApi();
 		if (web_api && web_api.GetRestContext()) {
-			m_RichText = new RichTextPrefab();
-			web_api.GetRestContext().GET(new ChangelogRestCallback(ScriptCaller.Create(SetListBoxInfo)), "api\/changelog\/latest");
-			AddContent(m_RichText);
+			web_api.GetRestContext().GET(new ChangelogRestCallback(ScriptCaller.Create(SetListBoxInfo), ScriptCaller.Create(OnChangelogLoadFailed)), "api\/changelog");
+		} else {
+			OnChangelogLoadFailed();
 		}
-		
-		m_TextBox = new MessageBoxPrefab("DayZ Editor is 100% free to use—and it always will be! But if you want to see even more awesome updates, consider joining our supporter community on Discord. Your support drives future content and improvements!");
-		m_NeverShowAgain = new CheckBoxPrefab("Don't Show Again", this, "m_NeverShowAnymore");
-		
-		AddContent(m_TextBox);
-		AddContent(m_NeverShowAgain);
-		
-		AddButton(new DialogButton("Support Dab!", "OnDonateButton"));
-		AddButton(new DialogButton("Join Discord", "OnDiscordButton"));
-		AddButton(new DialogButton("Close", "OnCloseButton"));
+
+		UpdateNavigationButtons();
 	}
-	
-	protected void SetListBoxInfo(Payload_Changelog pl)
+
+	protected void SetListBoxInfo(array<ref Payload_Changelog> payload)
 	{
+		if (!payload || payload.Count() == 0) {
+			RenderListMessage("No changelog entries available.");
+			return;
+		}
+
+		m_Changelogs = payload;
+		SortChangelogsById();
+		m_CurrentChangelogIndex = 0;
+		RenderCurrentChangelog();
+	}
+
+	protected void SortChangelogsById()
+	{
+		if (!m_Changelogs) {
+			return;
+		}
+
+		for (int i = 0; i < m_Changelogs.Count() - 1; i++) {
+			int max_index = i;
+			for (int j = i + 1; j < m_Changelogs.Count(); j++) {
+				if (m_Changelogs[j] && (!m_Changelogs[max_index] || m_Changelogs[j].Id > m_Changelogs[max_index].Id)) {
+					max_index = j;
+				}
+			}
+
+			if (max_index != i) {
+				Payload_Changelog changelog = m_Changelogs[i];
+				m_Changelogs[i] = m_Changelogs[max_index];
+				m_Changelogs[max_index] = changelog;
+			}
+		}
+	}
+
+	protected void RenderCurrentChangelog()
+	{
+		if (!m_Changelogs || m_Changelogs.Count() == 0) {
+			RenderListMessage("No changelog entries available.");
+			return;
+		}
+
+		if (m_CurrentChangelogIndex < 0) {
+			m_CurrentChangelogIndex = 0;
+		}
+
+		if (m_CurrentChangelogIndex >= m_Changelogs.Count()) {
+			m_CurrentChangelogIndex = m_Changelogs.Count() - 1;
+		}
+
+		Payload_Changelog pl = m_Changelogs[m_CurrentChangelogIndex];
 		if (!pl) {
+			RenderListMessage("Unable to load changelog.");
+			return;
+		}
+
+		if (!ListBox) {
 			return;
 		}
 		
-		m_RichText.ListBox.AddItem(string.Format("Change Log (%1)", pl.UploadDate), null, 0);
-		m_RichText.ListBox.AddItem(string.Format("Version %1", pl.UpdateVersion), null, 0);
-		m_RichText.ListBox.AddItem("", null, 0);
+		ListBox.ClearItems();
+		ListBox.AddItem(string.Format("Change Log (%1)", pl.UploadDate), null, 0);
+		ListBox.AddItem(string.Format("Version %1", pl.UpdateVersion), null, 0);
+		ListBox.AddItem("", null, 0);
+
 		foreach (string message_line: pl.ChangelogContent) {
-			m_RichText.ListBox.AddItem(message_line, null, 0);
+			ListBox.AddItem(message_line, null, 0);
 		}
-		
-		float s_x, s_y;
-		m_RichText.ListBox.GetScreenSize(s_x, s_y);
-		m_RichText.ListBox.SetScreenSize(s_x, pl.ChangelogContent.Count() * 22);
-		
+
+		UpdateNavigationButtons();
 		GetGame().GetCallQueue(CALL_CATEGORY_GUI).CallLater(SetupDialog, 10);
+	}
+
+	protected void RenderListMessage(string message)
+	{
+		if (!ListBox) {
+			return;
+		}
+
+		ListBox.ClearItems();
+		ListBox.AddItem(message, null, 0);
+		UpdateNavigationButtons();
+		GetGame().GetCallQueue(CALL_CATEGORY_GUI).CallLater(SetupDialog, 10);
+	}
+
+	protected void UpdateNavigationButtons()
+	{
+		int changelog_count = 0;
+		if (m_Changelogs) {
+			changelog_count = m_Changelogs.Count();
+		}
+
+		int current_page = m_CurrentChangelogIndex + 1;
+
+		if (PageIndicatorText) {
+			if (changelog_count > 0) {
+				PageIndicatorText.SetText(string.Format("%1 / %2", current_page, changelog_count));
+			} else {
+				PageIndicatorText.SetText("- / -");
+			}
+		}
+
+		if (PrevButton) {
+			PrevButton.Enable(changelog_count > 1 && current_page > 1);
+		}
+
+		if (NextButton) {
+			NextButton.Enable(changelog_count > 1 && current_page < changelog_count);
+		}
+	}
+
+	protected void OnChangelogLoadFailed()
+	{
+		RenderListMessage("Unable to load changelog.");
+	}
+
+	void OnPrevButton()
+	{
+		if (!m_Changelogs || m_CurrentChangelogIndex <= 0) {
+			UpdateNavigationButtons();
+			return;
+		}
+
+		m_CurrentChangelogIndex--;
+		RenderCurrentChangelog();
+	}
+
+	void OnNextButton()
+	{
+		if (!m_Changelogs || m_CurrentChangelogIndex >= m_Changelogs.Count() - 1) {
+			UpdateNavigationButtons();
+			return;
+		}
+
+		m_CurrentChangelogIndex++;
+		RenderCurrentChangelog();
 	}
 	
 	void OnCloseButton()
 	{
-		if (m_NeverShowAnymore) {
+		if (NeverShowAgainCheckBox && NeverShowAgainCheckBox.IsChecked()) {
 			GetEditor().GetSettings().VersionRequestedNotToSeeDonationDialog = Editor.VersionNumber;
 			GetEditor().GetSettings().Save();
 		}
@@ -110,5 +234,10 @@ class EditorOneTimeDonationDialog: EditorDialogBase
 	{
 		GetGame().OpenURL("https:\/\/discord.gg\/dayz-editor");
 		CloseDialog(DialogResult.OK);
+	}
+
+	override string GetLayoutFile()
+	{
+		return "DayZEditor/gui/Layouts/dialogs/EditorChangelogDialog.layout";
 	}
 }
